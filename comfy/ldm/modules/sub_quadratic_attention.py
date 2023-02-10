1a4edd19cd2cc82f1d7e0796181a826299f7304e
comfyanonymous <comfyanonymous@protonmail.com>
2023-02-10 11:47:41 -0500

    Fix overflow issue with inplace softmax.

--- comfy/ldm/modules/sub_quadratic_attention.py
+++ comfy/ldm/modules/sub_quadratic_attention.py
@@ -158,6 +158,7 @@ def _get_attention_scores_no_kv_chunking(
         del attn_scores
     except OOM_EXCEPTION:
         print("ran out of memory while running softmax in  _get_attention_scores_no_kv_chunking, trying slower in place softmax instead")
+        attn_scores -= attn_scores.max(dim=-1, keepdim=True).values
         torch.exp(attn_scores, out=attn_scores)
         summed = torch.sum(attn_scores, dim=-1, keepdim=True)
         attn_scores /= summed
