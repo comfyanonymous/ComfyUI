6a8cd6f8acc0fe4a7ac2462dd6fb4f561b02ee0b
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-02-17 22:56:40 +0700

    .

--- comfy/annotator/canny/__init__.py
+++ comfy/annotator/canny/__init__.py
@@ -2,5 +2,5 @@ import cv2
 
 
 class CannyDetector:
-    def __call__(self, img, low_threshold, high_threshold):
-        return cv2.Canny(img, low_threshold, high_threshold)
+    def __call__(self, img, low_threshold, high_threshold, l2gradient):
+        return cv2.Canny(img, low_threshold, high_threshold, L2gradient=l2gradient)
