4ed39cf03847f85b80eb9bdb566615bea9e2a862
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2023-03-08 20:04:22 +0000

    Added invert scrolling setting

---
+++ web/extensions/core/invertMenuScrolling.js
@@ -0,0 +1,36 @@
+import { app } from "/scripts/app.js";
+
+// Inverts the scrolling of context menus
+
+const id = "Comfy.InvertMenuScrolling";
+const ctxMenu = LiteGraph.ContextMenu;
+app.registerExtension({
+	name: id,
+	init() {
+		const replace = () => {
+			LiteGraph.ContextMenu = function (values, options) {
+				options = options || {};
+				if (options.scroll_speed) {
+					options.scroll_speed *= -1;
+				} else {
+					options.scroll_speed = -0.1;
+				}
+				return ctxMenu.call(this, values, options);
+			};
+			LiteGraph.ContextMenu.prototype = ctxMenu.prototype;
+		};
+		app.ui.settings.addSetting({
+			id,
+			name: "Invert Menu Scrolling",
+			type: "boolean",
+			defaultValue: false,
+			onChange(value) {
+				if (value) {
+					replace();
+				} else {
+					LiteGraph.ContextMenu = ctxMenu;
+				}
+			},
+		});
+	},
+});
