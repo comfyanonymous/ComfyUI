b566f23b945962a4e101440dc1425a5d58f839a2
omar92 <ooomar92@gmail.com>
2023-04-04 19:08:41 +0200

    try to solve the input equation

--- web/lib/litegraph.core.js
+++ web/lib/litegraph.core.js
@@ -9994,6 +9994,12 @@ LGraphNode.prototype.executeAction = function(action)
 						var delta = x < 40 ? -1 : x > widget_width - 40 ? 1 : 0;
 						if (event.click_time < 200 && delta == 0) {
 							this.prompt("Value",w.value,function(v) {
+                                    
+                                    try {//solve the equation if possible
+                                        v = eval(v);
+                                    } catch (error) {
+                                    }
+
 									this.value = Number(v);
 									inner_value_change(this, this.value);
 								}.bind(w),
