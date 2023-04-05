3536a7c8d148f738d30a375eab859c74da91a25a
missionfloyd <missionfloyd@users.noreply.github.com>
2023-04-05 08:57:44 -0600

    Put drag icon back

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -415,6 +415,7 @@ export class ComfyUI {
 
 		this.menuContainer = $el("div.comfy-menu", { parent: document.body }, [
 			$el("div.drag-handle", { style: { overflow: "hidden", position: "relative", width: "100%", cursor: "default" } }, [
+				$el("span.drag-handle"),
 				$el("span", { $: (q) => (this.queueSize = q) }),
 				$el("button.comfy-settings-btn", { textContent: "⚙️", onclick: () => this.settings.show() }),
 			]),
