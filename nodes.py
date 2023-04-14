f48f0872e2310b1650f798d02e94264cc06afd69
comfyanonymous <comfyanonymous@protonmail.com>
2023-04-14 00:21:01 -0400

    Refactor: move nodes_mask_convertion nodes to nodes_mask.

--- nodes.py
+++ nodes.py
@@ -1193,4 +1193,3 @@ def init_custom_nodes():
     load_custom_node(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy_extras"), "nodes_upscale_model.py"))
     load_custom_node(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy_extras"), "nodes_post_processing.py"))
     load_custom_node(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy_extras"), "nodes_mask.py"))
-    load_custom_node(os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy_extras"), "nodes_mask_conversion.py"))
