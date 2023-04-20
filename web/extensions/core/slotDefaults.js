5229c1f972b4130d5d0ddc19362604c6ec57d1fd
omar92 <ooomar92@gmail.com>
2023-04-20 21:13:14 +0200

    add option on the settings to change the number of the suggestions

--- web/extensions/core/slotDefaults.js
+++ web/extensions/core/slotDefaults.js
@@ -4,46 +4,69 @@ import { ComfyWidgets } from "/scripts/widgets.js";
 
 app.registerExtension({
 	name: "Comfy.SlotDefaults",
+	suggestionsNumber: null,
 	init() {
 		LiteGraph.middle_click_slot_add_default_node = true;
+		this.suggestionsNumber = app.ui.settings.addSetting({
+			id: "Comfy.NodeSuggestions.number",
+			name: "number of nodes suggestions",
+			type: "slider",
+			attrs: {
+				min: 1,
+				max: 100,
+				step: 1,
+			},
+			defaultValue: 5,
+			onChange: (newVal, oldVal) => {
+				this.setDefaults(newVal);
+			}
+		});
 	},
+	slot_types_default_out: {},
+	slot_types_default_in: {},
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
-		var nodeId = nodeData.name;
+				var nodeId = nodeData.name;
 		var inputs = [];
-		//if (nodeData["input"]["optional"] != undefined) {
-		//	inputs = Object.assign({}, nodeData["input"]["required"], nodeData["input"]["optional"]);
-		//} else {
-		inputs = nodeData["input"]["required"]; //only show required inputs to reduce the mess also not logica to create node with optional inputs
-		//}
+		inputs = nodeData["input"]["required"]; //only show required inputs to reduce the mess also not logical to create node with optional inputs
 		for (const inputKey in inputs) {
 			var input = (inputs[inputKey]);
-			//make sure input[0] is a string
 			if (typeof input[0] !== "string") continue;
 
-			//	for (const slotKey in inputs[inputKey]) {
 			var type = input[0]
 			if (type in ComfyWidgets) {
 				var customProperties = input[1]
-				//console.log(customProperties)
 				if (!(customProperties?.forceInput)) continue; //ignore widgets that don't force input
 			}
 
-			if (!(type in LiteGraph.slot_types_default_out)) {
-				LiteGraph.slot_types_default_out[type] = ["Reroute"];
+			if (!(type in this.slot_types_default_out)) {
+				this.slot_types_default_out[type] = ["Reroute"];
 			}
-			if (LiteGraph.slot_types_default_out[type].includes(nodeId)) continue;
-			LiteGraph.slot_types_default_out[type].push(nodeId);
-			//	}
-		}
+			if (this.slot_types_default_out[type].includes(nodeId)) continue;
+			this.slot_types_default_out[type].push(nodeId);
+		} 
 
 		var outputs = nodeData["output"];
 		for (const key in outputs) {
 			var type = outputs[key];
-			if (!(type in LiteGraph.slot_types_default_in)) {
-				LiteGraph.slot_types_default_in[type] = ["Reroute"];// ["Reroute", "Primitive"];  primitive doesn't always work :'()
+			if (!(type in this.slot_types_default_in)) {
+				this.slot_types_default_in[type] = ["Reroute"];// ["Reroute", "Primitive"];  primitive doesn't always work :'()
 			}
-			LiteGraph.slot_types_default_in[type].push(nodeId);
-		}
 
+			this.slot_types_default_in[type].push(nodeId);
+		}
+		var maxNum = this.suggestionsNumber ? this.suggestionsNumber.value : 5;
+		this.setDefaults(maxNum);
 	},
+	setDefaults(maxNum) {
+
+		LiteGraph.slot_types_default_out = {};
+		LiteGraph.slot_types_default_in = {};
+
+		for (const type in this.slot_types_default_out) {
+			LiteGraph.slot_types_default_out[type] = this.slot_types_default_out[type].slice(0, maxNum);
+		}
+		for (const type in this.slot_types_default_in) {
+			LiteGraph.slot_types_default_in[type] = this.slot_types_default_in[type].slice(0, maxNum);
+		}
+	}
 });
