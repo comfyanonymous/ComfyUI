ab9a9deff48b5780bd105dfd6d19f5f8333ef608
comfyanonymous <comfyanonymous@protonmail.com>
2023-04-28 09:03:39 -0400

    Fix nightly CI builds.
    No cu121 builds for windows yet.

--- .ci/nightly/update_windows/update_comfyui_and_python_dependencies.bat
+++ .ci/nightly/update_windows/update_comfyui_and_python_dependencies.bat
@@ -1,3 +1,3 @@
 ..\python_embeded\python.exe .\update.py ..\ComfyUI\
-..\python_embeded\python.exe -s -m pip install --upgrade --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121 -r ../ComfyUI/requirements.txt pygit2
+..\python_embeded\python.exe -s -m pip install --upgrade --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cu118 -r ../ComfyUI/requirements.txt pygit2
 pause
