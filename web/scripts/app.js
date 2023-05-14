e7b9d2c02cffd59fecca4ee617137ea38641078a
comfyanonymous <comfyanonymous@protonmail.com>
2023-05-14 01:30:58 -0400

    /prompt endpoint error is now in json format.

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1222,7 +1222,7 @@ export class ComfyApp {
 					try {
 						await api.queuePrompt(number, p);
 					} catch (error) {
-						this.ui.dialog.show(error.response || error.toString());
+						this.ui.dialog.show(error.response.error || error.toString());
 						break;
 					}
 
