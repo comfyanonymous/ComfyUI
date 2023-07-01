4c37e6f1125ef2e30b8ca27d8f0d309fa2bf8832
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-07-01 10:43:59 +0900

    object storage application fix

--- worklist_execution.py
+++ worklist_execution.py
@@ -457,17 +457,17 @@ class PromptExecutor:
             d = self.outputs.pop(o)
             del d
 
-        # Next, remove the subsequent outputs since they will not be executed
-        to_delete = []
-        for o in self.outputs:
-            if (o not in current_outputs) and (o not in executed):
-                to_delete += [o]
-                if o in self.old_prompt:
-                    d = self.old_prompt.pop(o)
-                    del d
-        for o in to_delete:
-            d = self.outputs.pop(o)
-            del d
+            to_delete = []
+            for o in self.object_storage:
+                if o[0] not in prompt:
+                    to_delete += [o]
+                else:
+                    p = prompt[o[0]]
+                    if o[1] != p['class_type']:
+                        to_delete += [o]
+            for o in to_delete:
+                d = self.object_storage.pop(o)
+                del d
 
     def execute(self, prompt, prompt_id, extra_data={}, execute_outputs=[]):
         nodes.interrupt_processing(False)
