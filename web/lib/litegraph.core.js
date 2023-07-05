398f719bb1e0fbb74d729fe208567395875be337
jnyfil <jnyfulfill@gmail.com>
2023-07-05 18:15:00 +0900

    Fix Auto-open bug

--- web/lib/litegraph.core.js
+++ web/lib/litegraph.core.js
@@ -13792,7 +13792,7 @@ LGraphNode.prototype.executeAction = function(action)
         if (!disabled) {
             element.addEventListener("click", inner_onclick);
         }
-        if (options.autoopen) {
+        if (options.autoopen && !disabled) {
 			LiteGraph.pointerListenerAdd(element,"enter",inner_over);
         }
 
