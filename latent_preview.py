55fadce8b2667a33ce94a712e072342b0df31a00
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-07-08 15:23:21 +0900

    prevent breaking old Pillow users

--- latent_preview.py
+++ latent_preview.py
@@ -16,7 +16,12 @@ class LatentPreviewer:
     def decode_latent_to_preview_image(self, preview_format, x0):
         preview_image = self.decode_latent_to_preview(x0)
 
-        preview_image = ImageOps.contain(preview_image, (MAX_PREVIEW_RESOLUTION, MAX_PREVIEW_RESOLUTION), Image.Resampling.LANCZOS)
+        if hasattr(Image, 'Resampling'):
+            resampling = Image.Resampling.LANCZOS
+        else:
+            resampling = Image.ANTIALIAS
+
+        preview_image = ImageOps.contain(preview_image, (MAX_PREVIEW_RESOLUTION, MAX_PREVIEW_RESOLUTION), resampling)
 
         preview_type = 1
         if preview_format == "JPEG":
