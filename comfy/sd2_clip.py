f67f1c99b8e03285a6a8c490888b6e47058482fd
comfyanonymous <comfyanonymous@protonmail.com>
2023-07-15 01:10:33 -0400

    Fix CLIPSetLastLayer not reverting when removed.

--- comfy/sd2_clip.py
+++ comfy/sd2_clip.py
@@ -4,20 +4,13 @@ import os
 
 class SD2ClipModel(sd1_clip.SD1ClipModel):
     def __init__(self, arch="ViT-H-14", device="cpu", max_length=77, freeze=True, layer="penultimate", layer_idx=None, textmodel_path=None):
+        if layer == "penultimate":
+            layer="hidden"
+            layer_idx=23
+
         textmodel_json_config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sd2_clip_config.json")
-        super().__init__(device=device, freeze=freeze, textmodel_json_config=textmodel_json_config, textmodel_path=textmodel_path)
+        super().__init__(device=device, freeze=freeze, layer=layer, layer_idx=layer_idx, textmodel_json_config=textmodel_json_config, textmodel_path=textmodel_path)
         self.empty_tokens = [[49406] + [49407] + [0] * 75]
-        if layer == "last":
-            pass
-        elif layer == "penultimate":
-            layer_idx = -1
-            self.clip_layer(layer_idx)
-        elif self.layer == "hidden":
-            assert layer_idx is not None
-            assert abs(layer_idx) < 24
-            self.clip_layer(layer_idx)
-        else:
-            raise NotImplementedError()
 
     def clip_layer(self, layer_idx):
         if layer_idx < 0:
