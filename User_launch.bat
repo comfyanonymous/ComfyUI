b9e929dd3305829706d07bd150d855e6f15cddb0
Emser <cullenemmet@gmail.com>
2023-07-27 07:53:01 +0100

    Create User_launch.bat

---
+++ User_launch.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Set the path to the ComfyUI main.py script.
+set SCRIPT_PATH=C:\Users\culle\Documents\ComfyUI\ComfyUI-master\main.py
+
+rem Run the Python script.
+python %SCRIPT_PATH%
