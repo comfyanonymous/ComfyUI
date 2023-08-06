1504f55cdf6c9d972af5a11a5c40fc4a7df95df3
Barry Downes <barry.downes@gmail.com>
2023-08-06 15:32:19 +1000

    Fix: Updated strip_c_comments function didn't work.
    In regex, "." is literal inside a character class.

--- comfy/comments.py
+++ comfy/comments.py
@@ -4,7 +4,8 @@ import re
 def strip_c_comments(text, strict=True):
     # Processes the text and strips out any C-style block "/* ... */" or line "// ..." comments found.
     # from old dynamicPrompts.js: return str.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'');
-    return re.sub(r'/\*[.\n]*?(?:\*/|$)|//.*', '', text)
+    text = re.sub(r'/\*.*?(?:\*/|$)|//[^\n]*', '', text, flags=re.DOTALL)
+    return text
 
 def strip_hash_comments(text, strict=True):
     # Processes the text and strips out any hash "# ... " comments found.
