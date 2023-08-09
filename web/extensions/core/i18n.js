c1da0d6cc1bb7eb0e439f884489a88c8ddf1685e
xbol0 <xbolo@duck.com>
2023-08-09 05:48:41 +0000

    update translation fields

--- web/extensions/core/i18n.js
+++ web/extensions/core/i18n.js
@@ -9,30 +9,43 @@ app.registerExtension({
             if ("input" in defs[k] && defs[k].input.required) {
                 for (const i in defs[k].input.required) {
                     if (defs[k].input.required[i].length > 1) {
-                        defs[k].input.required[i][1].label = i18next.t(`node.input.${k}.${i}`)
+                        defs[k].input.required[i][1].label = i18next.t(`node.input.${i}`)
                     } else {
-                        defs[k].input.required[i].push({ label: i18next.t(`node.input.${k}.${i}`) })
+                        defs[k].input.required[i].push({ label: i18next.t(`node.input.${i}`) })
                     }
                 }
+            }
+            if ("input" in defs[k] && defs[k].input.optional) {
+                for (const i in defs[k].input.optional) {
+                    if (defs[k].input.optional[i].length > 1) {
+                        defs[k].input.optional[i][1].label = i18next.t(`node.input.${i}`)
+                    } else {
+                        defs[k].input.optional[i].push({ label: i18next.t(`node.input.${i}`) })
+                    }
+                }
+            }
+
+            if (!("output_name" in defs[k]) && defs[k].output && defs[k].output.length > 0) {
+                defs[k].output_name = defs[k].output.map(i => i18next.t(`node.output.${i}`))
             }
         }
     },
     nodeCreated(node) {
         if ("inputs" in node) {
             for (const item of node.inputs) {
-                item.label = i18next.t(`node.input.${node.comfyClass}.${item.name}`)
+                item.label = i18next.t(`node.input.${item.name}`)
             }
         }
 
         if ("widgets" in node) {
             for (const item of node.widgets) {
-                item.label = i18next.t(`node.input.${node.comfyClass}.${item.name}`)
+                item.label = i18next.t(`node.input.${item.name}`)
             }
         }
 
         if ("outputs" in node) {
             for (const item of node.outputs) {
-                item.label = i18next.t(`node.output.${node.comfyClass}.${item.name}`)
+                item.label = i18next.t(`node.output.${item.name}`)
             }
         }
     },
@@ -41,6 +54,7 @@ app.registerExtension({
 
         for (const k in defs) {
             defs[k].category = i18next.t(`category.${defs[k].category}`)
+            defs[k].display_name = i18next.t(`node.title.${k}`)
         }
     }
 })
