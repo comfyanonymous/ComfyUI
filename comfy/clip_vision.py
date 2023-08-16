dcbf839d229e9bf51ef927c249c6ad46d808a838
comfyanonymous <comfyanonymous@protonmail.com>
2023-08-16 11:05:11 -0400

    Fix potential issue with batch size and clip vision.

--- comfy/clip_vision.py
+++ comfy/clip_vision.py
@@ -25,6 +25,8 @@ class ClipVisionModel():
 
     def encode_image(self, image):
         img = torch.clip((255. * image), 0, 255).round().int()
+        if len(img.shape) == 3:
+            img = [img]
         inputs = self.processor(images=img, return_tensors="pt")
         outputs = self.model(**inputs)
         return outputs
