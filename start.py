8480c5ff8f6f49fe339e6b5d7c838c3b928ab448
walkingwormfood <36175269+walkingwormfood@users.noreply.github.com>
2023-08-23 09:52:22 -0500

    Renamed main.py to start.py

--- start.py
+++ start.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
         def startup_server(address, port):
             import webbrowser
             if os.name == 'nt' and address == '0.0.0.0':
-                address = '127.0.0.1'
+                address = '0.0.0.0'
             webbrowser.open(f"http://{address}:{port}")
         call_on_start = startup_server
 
