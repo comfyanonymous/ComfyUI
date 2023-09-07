00721f9d6ad101a841158f67c4b49a26b6f713b7
comfyanonymous <comfyanonymous@protonmail.com>
2023-09-07 03:31:43 -0400

    Support DiffBIR SwinIR models.

--- comfy_extras/nodes_upscale_model.py
+++ comfy_extras/nodes_upscale_model.py
@@ -18,6 +18,8 @@ class UpscaleModelLoader:
     def load_model(self, model_name):
         model_path = folder_paths.get_full_path("upscale_models", model_name)
         sd = comfy.utils.load_torch_file(model_path, safe_load=True)
+        if "module.layers.0.residual_group.blocks.0.norm1.weight" in sd:
+            sd = comfy.utils.state_dict_prefix_replace(sd, {"module.":""})
         out = model_loading.load_state_dict(sd).eval()
         return (out, )
 
