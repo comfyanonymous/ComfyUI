400300c9dca7b98882717811d6dcc2da5d99ef6f
Chris <chris.goringe@gmail.com>
2023-09-11 16:07:13 +1000

    Make hideWidget safer

--- web/extensions/core/widgetInputs.js
+++ web/extensions/core/widgetInputs.js
@@ -9,6 +9,7 @@ function isConvertableWidget(widget, config) {
 }
 
 function hideWidget(node, widget, suffix = "") {
+	if (widget.type.includes(CONVERTED_TYPE)) { return; } // already hidden, don't do it again
 	widget.origType = widget.type;
 	widget.origComputeSize = widget.computeSize;
 	widget.origSerializeValue = widget.serializeValue;
