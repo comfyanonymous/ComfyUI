3f26d9e916122a47013903a4c66e1bbe7e45d502
Emser <cullenemmet@gmail.com>
2023-09-20 03:11:56 +0100

    Workflow tidy and launch bat update

--- User_launch.bat
+++ User_launch.bat
@@ -1,7 +1,8 @@
 @echo off
 
 rem Set the path to the ComfyUI main.py script.
-set SCRIPT_PATH=C:\Users\culle\Documents\ComfyUI\ComfyUI-master\main.py
+set SCRIPT_PATH=C:/Users/culle/Documents/ComfyUI2/ComfyUI/main.py
 
 rem Run the Python script.
 python %SCRIPT_PATH%
+pause
