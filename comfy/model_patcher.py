afa2399f79e84919645eb69cd8e1ef1d9f1d6bd1
comfyanonymous <comfyanonymous@protonmail.com>
2023-09-22 20:26:47 -0400

    Add a way to set output block patches to modify the h and hsp.

--- comfy/model_patcher.py
+++ comfy/model_patcher.py
@@ -88,6 +88,9 @@ class ModelPatcher:
     def set_model_attn2_output_patch(self, patch):
         self.set_model_patch(patch, "attn2_output_patch")
 
+    def set_model_output_block_patch(self, patch):
+        self.set_model_patch(patch, "output_block_patch")
+
     def model_patches_to(self, device):
         to = self.model_options["transformer_options"]
         if "patches" in to:
