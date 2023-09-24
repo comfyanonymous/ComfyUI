70c6e04ae4d283a0724a1de3eb2c0b7fdc6a270f
comfyanonymous <comfyanonymous@protonmail.com>
2023-09-24 13:27:57 -0400

    Fix typo.

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1322,6 +1322,7 @@ export class ComfyApp {
 		for (let n of graphData.nodes) {
 			// Patch T2IAdapterLoader to ControlNetLoader since they are the same node now
 			if (n.type == "T2IAdapterLoader") n.type = "ControlNetLoader";
+			if (n.type == "ConditioningAverage ") n.type = "ConditioningAverage"; //typo fix
 
 			// Find missing node types
 			if (!(n.type in LiteGraph.registered_node_types)) {
