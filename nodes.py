1d1621b03d4dea6b3b456eb2e0e4c8f7236179d7
comfyanonymous <comfyanonymous@protonmail.com>
2023-10-21 05:16:38 -0400

    HyperTile node, can be found in: _for_testing->HyperTile

--- nodes.py
+++ nodes.py
@@ -1796,7 +1796,8 @@ def init_custom_nodes():
         "nodes_clip_sdxl.py",
         "nodes_canny.py",
         "nodes_freelunch.py",
-        "nodes_custom_sampler.py"
+        "nodes_custom_sampler.py",
+        "nodes_hypertile.py",
     ]
 
     for node_file in extras_files:
