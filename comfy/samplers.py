5b4cacf352ed62f534646b57ec24852e2f98692d
comfyanonymous <comfyanonymous@protonmail.com>
2023-11-10 20:52:10 -0500

    Working RescaleCFG node.
    This was broken because of recent changes so I fixed it and moved it from
    the experiments repo.

--- comfy/samplers.py
+++ comfy/samplers.py
@@ -248,7 +248,7 @@ def sampling_function(model_function, x, timestep, uncond, cond, cond_scale, mod
 
         cond, uncond = calc_cond_uncond_batch(model_function, cond, uncond, x, timestep, max_total_area, model_options)
         if "sampler_cfg_function" in model_options:
-            args = {"cond": x - cond, "uncond": x - uncond, "cond_scale": cond_scale, "timestep": timestep, "input": x}
+            args = {"cond": x - cond, "uncond": x - uncond, "cond_scale": cond_scale, "timestep": timestep, "input": x, "sigma": timestep}
             return x - model_options["sampler_cfg_function"](args)
         else:
             return uncond + (cond - uncond) * cond_scale
