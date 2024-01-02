b576ad52303439f8e910bd9696e756da9095b82b
xiaoshuisheng <etaxiao@outlook.com>
2024-01-02 10:29:34 +0800

    [ADD] startup script for linux

---
+++ build_run_executor_dp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+service="aiyo_server_main"
+pids=$(ps -ef | grep "$service" | grep -v grep | awk '{print $2}')
+
+if [ -n "$pids" ]; then
+    for pid in $pids; do
+        kill -9 "$pid"
+    done
+fi
+
+
+VENV_DIR=venv
+REQUIREMENTS=requirements.txt
+
+echo Setting up virtual environment...
+if [ ! -d "$VENV_DIR" ]; then
+python3 -m venv "$VENV_DIR"
+fi
+. "$VENV_DIR/bin/activate"
+
+echo Installing requirements...
+pip3 install -r "$REQUIREMENTS"
+
+echo Setup complete.
+
+echo Running the application...
+. "$VENV_DIR/bin/activate"
+nohup python aiyo_executor_main.py --config "debug_dp" > nohup.out 2>&1 &
