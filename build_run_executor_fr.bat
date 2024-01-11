32874cc62ac2a6cc17ad08e93e7042746ff801cd
xiaoshuisheng <etaxiao@outlook.com>
2024-01-11 17:12:55 +0800

    [ADD] add config

---
+++ build_run_executor_fr.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+set VENV_DIR=venv
+set REQUIREMENTS=requirements.txt
+
+echo Setting up virtual environment...
+python -m venv %VENV_DIR%
+call %VENV_DIR%\Scripts\activate.bat
+
+pip3 show torch > nul
+if %errorlevel% neq 0 (
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+)
+
+echo Installing requirements...
+pip install -r %REQUIREMENTS%
+
+python aiyo_project_init.py
+
+echo Setup complete.
+
+echo Running the application...
+call %VENV_DIR%\Scripts\activate.bat
+
+
+python aiyo_executor_main.py --config "aiyoh_engine_fr"
