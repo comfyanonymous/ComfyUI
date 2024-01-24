d1533d9c0f1dde192f738ef1b745b15f49f41e02
comfyanonymous <comfyanonymous@protonmail.com>
2024-01-24 09:49:57 -0500

    Add experimental photomaker nodes.
    Put the model file in models/photomaker and use PhotoMakerLoader.
    
    Then use PhotoMakerEncode with the keyword "photomaker" to apply the image

--- folder_paths.py
+++ folder_paths.py
@@ -29,6 +29,8 @@ folder_names_and_paths["custom_nodes"] = ([os.path.join(base_path, "custom_nodes
 
 folder_names_and_paths["hypernetworks"] = ([os.path.join(models_dir, "hypernetworks")], supported_pt_extensions)
 
+folder_names_and_paths["photomaker"] = ([os.path.join(models_dir, "photomaker")], supported_pt_extensions)
+
 folder_names_and_paths["classifiers"] = ([os.path.join(models_dir, "classifiers")], {""})
 
 output_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
