2fb56544aa91bc3b06e1b13732a88f17be2e83e2
Loner <yinjiakang@gmail.com>
2024-01-31 23:33:09 +0800

    use local storage

--- web/scripts/utils.js
+++ web/scripts/utils.js
@@ -1,32 +1,11 @@
 import { $el } from "./ui.js";
 
-function setCookie(name, value, days) {
-	var expires = "";
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		expires = "; expires=" + date.toUTCString();
-	}
-	document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-	}
-	return null;
-}
-
 export function needLoadPrebuiltWorkflow(workflowId) {
-	var loaded = getCookie('PrebuiltWorkflowId' + workflowId);
+	var loaded = localStorage.getItem('PrebuiltWorkflowId' + workflowId);
 	if (loaded) {
 		return false
 	} else {
-		setCookie('PrebuiltWorkflowId' + workflowId, true, 999);
+		localStorage.setItem('PrebuiltWorkflowId' + workflowId, true);
 		return true
 	}
 }
