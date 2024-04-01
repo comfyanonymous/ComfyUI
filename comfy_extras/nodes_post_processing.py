9668b8ef1191482be56796c9dffca48d85b48462
Liu-XinYuan <879928098@qq.com>
2024-04-01 15:45:26 +0800

    fix

--- comfy_extras/nodes_post_processing.py
+++ comfy_extras/nodes_post_processing.py
@@ -108,7 +108,7 @@ class Blur:
         kernel = gaussian_kernel(kernel_size, sigma, device=image.device).repeat(channels, 1, 1).unsqueeze(1)
 
         image = image.permute(0, 3, 1, 2) # Torch wants (B, C, H, W) we use (B, H, W, C)
-        padded_image = F.pad(image, (blur_radius,blur_radius,blur_radius,blur_radius), 'reflect')
+        padded_image = F.pad(image, (blur_radius,blur_radius,blur_radius,blur_radius), 'reflect').float()
         blurred = F.conv2d(padded_image, kernel, padding=kernel_size // 2, groups=channels)[:,:,blur_radius:-blur_radius, blur_radius:-blur_radius]
         blurred = blurred.permute(0, 2, 3, 1)
 
