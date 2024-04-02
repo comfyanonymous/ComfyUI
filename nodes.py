f46cdd07f7c15420f0fdbe9fa858ec505558f52d
Weixuan Fu <weixuanf@outlook.com>
2024-04-02 15:27:46 +0800

    split ddb put logic into scanner/

--- nodes.py
+++ nodes.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 import torch
 
 import os
@@ -1875,8 +1874,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 EXTENSION_WEB_DIRS = {}
-from scanner.ddb_utils import put_node_package_ddb, put_node_ddb
-from scanner.githubUtils import get_repo_user_and_name
+from scanner.ddb_utils import write_to_db_record
+
 cur_node_package = {}
 def load_custom_node(module_path, ignore=set()):
     global cur_node_package
@@ -1921,68 +1920,6 @@ def load_custom_node(module_path, ignore=set()):
         cur_node_package['importError'] = error_details
         return False
 
-# copied from server.py
-def node_info(node_class:str):
-    obj_class = NODE_CLASS_MAPPINGS[node_class]
-    info = {}
-    info['input'] = obj_class.INPUT_TYPES()
-    info['output'] = obj_class.RETURN_TYPES
-    info['output_is_list'] = obj_class.OUTPUT_IS_LIST if hasattr(obj_class, 'OUTPUT_IS_LIST') else [False] * len(obj_class.RETURN_TYPES)
-    info['output_name'] = obj_class.RETURN_NAMES if hasattr(obj_class, 'RETURN_NAMES') else info['output']
-    info['name'] = node_class
-    info['display_name'] = NODE_DISPLAY_NAME_MAPPINGS[node_class] if node_class in NODE_DISPLAY_NAME_MAPPINGS.keys() else node_class
-    info['description'] = obj_class.DESCRIPTION if hasattr(obj_class,'DESCRIPTION') else ''
-    info['category'] = 'sd'
-    if hasattr(obj_class, 'OUTPUT_NODE') and obj_class.OUTPUT_NODE == True:
-        info['output_node'] = True
-    else:
-        info['output_node'] = False
-
-    if hasattr(obj_class, 'CATEGORY'):
-        info['category'] = obj_class.CATEGORY
-    return info
-
-from  scanner.analyze_node_input import analyze_class
-
-
-def custom_serializer(obj):
-    """Convert non-serializable objects."""
-    if isinstance(obj, (list, tuple, set)):
-        return list(obj)  # Convert tuples and sets to lists
-    elif isinstance(obj, dict):
-        # Recursively apply to dictionary entries
-        return {str(key): custom_serializer(value) for key, value in obj.items()}
-    else:
-        return obj
-from scanner.controller import cur_git_repo
-
-# For COMFYUI BASE NODES
-def save_base_nodes_to_ddb():
-    baseNodeDefs = {}
-    for name in NODE_CLASS_MAPPINGS: 
-        paths = analyze_class(NODE_CLASS_MAPPINGS[name])
-        node_def = node_info(name)
-        data = {
-            "id": name+"~"+'comfyanonymous_ComfyUI',
-            "nodeType": name, 
-            "nodeDef": json.dumps(node_def), 
-            "packageID": 'comfyanonymous_ComfyUI',
-            "gitRepo": 'comfyanonymous/ComfyUI'}
-        baseNodeDefs[name] = node_def
-        if paths is not None and len(paths) > 0:
-            data['folderPaths'] = json.dumps(paths, default=custom_serializer)
-        put_node_ddb(data)
-    put_node_package_ddb({
-                    'id': 'comfyanonymous_ComfyUI',
-                    'gitRepo': "comfyanonymous/ComfyUI",
-                    'gitHtmlUrl': 'https://github.com/comfyanonymous/ComfyUI',
-                    'nameID': 'ComfyUI',
-                    'authorID': 'admin',
-                    'status': 'IMPORT_SUCCESS',
-                    'defaultBranch': 'master',
-                    'totalNodes':len(NODE_CLASS_MAPPINGS),
-                    'nodeDefs': json.dumps(baseNodeDefs)
-                })
 # save_base_nodes_to_ddb()
 def load_custom_nodes():
     print('🤔🤔🤔 load cusotm nodes')
@@ -2002,45 +1939,18 @@ def load_custom_nodes():
             prev_nodes = set(NODE_CLASS_MAPPINGS.keys())
             success = load_custom_node(module_path, base_node_names)
             print("imported nodes: success",success)
-            # WRITE TO DDB
-            if 'ComfyUI-Manager' in possible_module:
-                continue
-            nodes_count = len(NODE_CLASS_MAPPINGS) - len(prev_nodes)
             node_import_times.append((time.perf_counter() - time_before, module_path, success))
-            username, repo_name, default_branch_name = get_repo_user_and_name(module_path)
-            print('🍻 cur_node_package',cur_node_package)
-            packageID = username + '_' + repo_name
-            custom_node_defs = {}
-            for name in NODE_CLASS_MAPPINGS:
-                try:
-                    if name not in prev_nodes: 
-                        paths = analyze_class(NODE_CLASS_MAPPINGS[name])
-                        node_def = node_info(name)
-                        data = {
-                            "id": name+"~"+packageID,
-                            "nodeType": name, 
-                            "nodeDef": json.dumps(node_def), 
-                            "packageID": packageID,
-                            "gitRepo": username + '/' + repo_name}
-                        custom_node_defs[name] = node_def
-                        if paths is not None and len(paths) > 0:
-                            data['folderPaths'] = json.dumps(paths, default=custom_serializer)
-                        put_node_ddb(data)
-                except Exception as e:
-                    print("❌analyze imported node: error",e)
-            put_node_package_ddb({
-                **cur_node_package,
-                'id': packageID,
-                'gitRepo': username + '/' + repo_name,
-                'gitHtmlUrl': 'https://github.com/'+username + '/' + repo_name,
-                'nameID': repo_name,
-                'authorID': 'admin',
-                'status': 'IMPORT_'+ ('SUCCESS' if success else 'FAILED'),
-                'defaultBranch': default_branch_name,
-                'totalNodes':nodes_count,
-                "importTime": Decimal(str( time.perf_counter() - time_before)),
-                'nodeDefs': json.dumps(custom_node_defs)
-            })
+            input_dict = {
+                'NODE_CLASS_MAPPINGS': NODE_CLASS_MAPPINGS,
+                'NODE_DISPLAY_NAME_MAPPINGS': NODE_DISPLAY_NAME_MAPPINGS,
+                'cur_node_package': cur_node_package,
+                'module_path': module_path,
+                'prev_nodes': prev_nodes,
+                'success': success,
+                'time_before': time_before
+            }
+            write_to_db_record(input_dict)
+
     if len(node_import_times) > 0:
         print("\nImport times for custom nodes:")
         for n in sorted(node_import_times):
--- scanner/ddb_utils.py
+++ scanner/ddb_utils.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import boto3
 from .githubUtils import get_github_repo_stars
 import json
+import aiohttp
 
 scanner_path = os.path.dirname(__file__)
 root_path = os.path.dirname(os.path.dirname(scanner_path))
@@ -14,8 +15,8 @@ load_dotenv(os.path.join(root_path, '.env.local'))
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_region = os.getenv('AWS_REGION')
-node_table_name = "ComfyNode" + os.getenv('DDB_TABLE_POSTFIX')# DDB_TABLE_CUSTOM_NODE
-package_table_name = "ComfyNodePackage" + os.getenv('DDB_TABLE_POSTFIX')
+node_table_name = "Node" + os.getenv('DDB_TABLE_POSTFIX')# DDB_TABLE_CUSTOM_NODE
+package_table_name = "NodePackage" + os.getenv('DDB_TABLE_POSTFIX')
 log_file = os.path.join(root_path, 'log.txt')
 
 if not aws_access_key_id or not aws_secret_access_key:
@@ -119,4 +120,145 @@ def put_node_ddb(item):
         return item
     except Exception as e:
         print("❌🔴Error adding package item to DynamoDB:", e)
+        return None
+
+######v3
+
+def custom_serializer(obj):
+    """Convert non-serializable objects."""
+    if isinstance(obj, (list, tuple, set)):
+        return list(obj)  # Convert tuples and sets to lists
+    elif isinstance(obj, dict):
+        # Recursively apply to dictionary entries
+        return {str(key): custom_serializer(value) for key, value in obj.items()}
+    else:
+        return obj
+
+from .githubUtils import get_repo_user_and_name
+from decimal import Decimal
+import time
+from  scanner.analyze_node_input import analyze_class
+
+def write_to_db_record(input_dict): 
+    # WRITE TO DDB
+    NODE_CLASS_MAPPINGS = input_dict['NODE_CLASS_MAPPINGS']
+    NODE_DISPLAY_NAME_MAPPINGS = input_dict['NODE_DISPLAY_NAME_MAPPINGS']
+    cur_node_package = input_dict['cur_node_package']
+    module_path = input_dict['module_path']
+    prev_nodes = input_dict['prev_nodes']
+    success = input_dict['success']
+    time_before = input_dict['time_before']
+    if 'ComfyUI-Manager' in module_path:
+        return
+    nodes_count = len(NODE_CLASS_MAPPINGS) - len(prev_nodes)
+    import_time = time.perf_counter() - time_before
+    
+    username, repo_name, default_branch_name = get_repo_user_and_name(module_path)
+    print('🍻 cur_node_package',cur_node_package)
+    packageID = username + '_' + repo_name
+    custom_node_defs = {}
+    for name in NODE_CLASS_MAPPINGS:
+        try:
+            if name not in prev_nodes: 
+                paths = analyze_class(NODE_CLASS_MAPPINGS[name])
+                # all_node = fetch_node_info()
+                node_def = node_info(input_dict, name)
+                data = {
+                    "id": name+"~"+packageID,
+                    "nodeType": name, 
+                    "nodeDef": json.dumps(node_def), 
+                    "packageID": packageID,
+                    "gitRepo": username + '/' + repo_name}
+                custom_node_defs[name] = node_def
+                if paths is not None and len(paths) > 0:
+                    data['folderPaths'] = json.dumps(paths, default=custom_serializer)
+                put_node_ddb(data)
+        except Exception as e:
+            print("❌analyze imported node: error",e)
+    put_node_package_ddb({
+        **cur_node_package,
+        'id': packageID,
+        'gitRepo': username + '/' + repo_name,
+        'gitHtmlUrl': 'https://github.com/'+username + '/' + repo_name,
+        'nameID': repo_name,
+        'authorID': 'admin',
+        'status': 'IMPORT_'+ ('SUCCESS' if success else 'FAILED'),
+        'defaultBranch': default_branch_name,
+        'totalNodes':nodes_count,
+        "importTime": Decimal(str(import_time)),
+        'nodeDefs': json.dumps(custom_node_defs)
+    })
+
+# For COMFYUI BASE NODES
+def save_base_nodes_to_ddb(NODE_CLASS_MAPPINGS):
+    baseNodeDefs = {}
+    for name in NODE_CLASS_MAPPINGS: 
+        paths = analyze_class(NODE_CLASS_MAPPINGS[name])
+        node_def = node_info(name)
+        data = {
+            "id": name+"~"+'comfyanonymous_ComfyUI',
+            "nodeType": name, 
+            "nodeDef": json.dumps(node_def), 
+            "packageID": 'comfyanonymous_ComfyUI',
+            "gitRepo": 'comfyanonymous/ComfyUI'}
+        baseNodeDefs[name] = node_def
+        if paths is not None and len(paths) > 0:
+            data['folderPaths'] = json.dumps(paths, default=custom_serializer)
+        put_node_ddb(data)
+    put_node_package_ddb({
+                    'id': 'comfyanonymous_ComfyUI',
+                    'gitRepo': "comfyanonymous/ComfyUI",
+                    'gitHtmlUrl': 'https://github.com/comfyanonymous/ComfyUI',
+                    'nameID': 'ComfyUI',
+                    'authorID': 'admin',
+                    'status': 'IMPORT_SUCCESS',
+                    'defaultBranch': 'master',
+                    'totalNodes':len(NODE_CLASS_MAPPINGS),
+                    'nodeDefs': json.dumps(baseNodeDefs)
+                })
+
+# copied from server.py
+def node_info(input_dict, node_class:str):
+    NODE_CLASS_MAPPINGS = input_dict['NODE_CLASS_MAPPINGS']
+    NODE_DISPLAY_NAME_MAPPINGS = input_dict['NODE_DISPLAY_NAME_MAPPINGS']
+    obj_class = NODE_CLASS_MAPPINGS[node_class]
+    info = {}
+    info['input'] = obj_class.INPUT_TYPES()
+    info['output'] = obj_class.RETURN_TYPES
+    info['output_is_list'] = obj_class.OUTPUT_IS_LIST if hasattr(obj_class, 'OUTPUT_IS_LIST') else [False] * len(obj_class.RETURN_TYPES)
+    info['output_name'] = obj_class.RETURN_NAMES if hasattr(obj_class, 'RETURN_NAMES') else info['output']
+    info['name'] = node_class
+    info['display_name'] = NODE_DISPLAY_NAME_MAPPINGS[node_class] if node_class in NODE_DISPLAY_NAME_MAPPINGS.keys() else node_class
+    info['description'] = obj_class.DESCRIPTION if hasattr(obj_class,'DESCRIPTION') else ''
+    info['category'] = 'sd'
+    if hasattr(obj_class, 'OUTPUT_NODE') and obj_class.OUTPUT_NODE == True:
+        info['output_node'] = True
+    else:
+        info['output_node'] = False
+
+    if hasattr(obj_class, 'CATEGORY'):
+        info['category'] = obj_class.CATEGORY
+    return info
+
+
+import json
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+
+def fetch_node_info():
+    url = "http://localhost:8188/object_info"
+    
+    try:
+        with urlopen(url) as response:
+            if response.status == 200:
+                data = json.loads(response.read().decode())
+                return data
+            else:
+                print(f"Failed to fetch node info. Status code: {response.status}")
+                return None
+    except HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
+        return None
+    except URLError as e:
+        print(f"URL Error: {e.reason}")
         return None
