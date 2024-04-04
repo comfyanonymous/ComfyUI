125a62efe29f86f4fc0d7ec67ed91aa4631f0391
comfyanonymous <comfyanonymous@protonmail.com>
2024-04-04 15:06:17 -0400

    Add InstructPixToPixConditioning node.

--- nodes.py
+++ nodes.py
@@ -1965,6 +1965,7 @@ def init_custom_nodes():
         "nodes_morphology.py",
         "nodes_stable_cascade.py",
         "nodes_differential_diffusion.py",
+        "nodes_ip2p.py",
     ]
 
     import_failed = []
