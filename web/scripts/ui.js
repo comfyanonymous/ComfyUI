schwaai/NodeGraphDev

Fix some errors

web/scripts/ui.js
@@ -411,14 +411,18 @@ class ComfyList {
 
 	async show() {
 		this.element.style.display = "block";
-		this.button.textContent = "Close";
+                if (this.button) {
+			this.button.textContent = "Close";
+                }
 
 		await this.load();
 	}
 
 	hide() {
 		this.element.style.display = "none";
-		this.button.textContent = "See " + this.#text;
+		if (this.button) {
+			this.button.textContent = "See " + this.#text;
+		}
 	}
 
 	toggle() {
@@ -575,6 +579,7 @@ export class ComfyUI {
                                         };
                                         list[name] = new_item;
                                         save_saved(list);
+                                        this.saved.update();
                                 },
                         }),
 			$el("button", {
@@ -620,6 +625,7 @@ export class ComfyUI {
 			}}),
 		]);
                 this.saved.show();
+                this.saved.update();
 
 		dragElement(this.menuContainer, this.settings);
 

comfyanonymous/ComfyUI

web/scripts/ui.js
@@ -263,14 +263,18 @@ class ComfyList {
 
 	async show() {
 		this.element.style.display = "block";
-		this.button.textContent = "Close";
+                if (this.button) {
+			this.button.textContent = "Close";
+                }
 
 		await this.load();
 	}
 
 	hide() {
 		this.element.style.display = "none";
-		this.button.textContent = "View " + this.#text;
+		if (this.button) {
+			this.button.textContent = "View " + this.#text;
+		}
 	}
 
 	toggle() {
@@ -545,6 +549,7 @@ export class ComfyUI {
                                         };
                                         list[name] = new_item;
                                         save_saved(list);
+                                        this.saved.update();
                                 },
                         }),
 			$el("button", {
@@ -632,6 +637,7 @@ export class ComfyUI {
 			}),
 		]);
                 this.saved.show();
+                this.saved.update();
 
 		const devMode = this.settings.addSetting({
 			id: "Comfy.DevMode",