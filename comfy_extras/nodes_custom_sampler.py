b7d06aa5a2492b61d336e00991a8ae96f5ed134f
comfyanonymous <comfyanonymous@protonmail.com>
2024-05-01 16:57:10 -0400

    Don't load the model in SDTurboScheduler

--- comfy_extras/nodes_custom_sampler.py
+++ comfy_extras/nodes_custom_sampler.py
@@ -107,8 +107,7 @@ class SDTurboScheduler:
     def get_sigmas(self, model, steps, denoise):
         start_step = 10 - int(10 * denoise)
         timesteps = torch.flip(torch.arange(1, 11) * 100 - 1, (0,))[start_step:start_step + steps]
-        comfy.model_management.load_models_gpu([model])
-        sigmas = model.model.model_sampling.sigma(timesteps)
+        sigmas = model.get_model_object("model_sampling").sigma(timesteps)
         sigmas = torch.cat([sigmas, sigmas.new_zeros([1])])
         return (sigmas, )
 
