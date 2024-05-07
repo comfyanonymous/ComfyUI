c0ec2dc090bf592d2bcefc61cedcc46bb3e9b741
shawnington <88048838+shawnington@users.noreply.github.com>
2024-05-07 12:29:18 -0700

    Corrected comment in issue #s fixed.

--- node_helpers.py
+++ node_helpers.py
@@ -14,7 +14,7 @@ def pillow(fn, arg):
     prev_value = None
     try:
         x = fn(arg)
-    except (OSError, UnidentifiedImageError, ValueError): #PIL issues #4472 , #3416, and #2445
+    except (OSError, UnidentifiedImageError, ValueError): #PIL issues #4472 and #2445, also fixes ComfyUI issue #3416
         prev_value = ImageFile.LOAD_TRUNCATED_IMAGES
         ImageFile.LOAD_TRUNCATED_IMAGES = True
         x = fn(arg)
