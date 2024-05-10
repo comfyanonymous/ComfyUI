4f63ee99f185db316a017520570310d094efffba
comfyanonymous <comfyanonymous@protonmail.com>
2024-05-10 17:30:52 -0400

    Add a button to reset the view.

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -597,14 +597,21 @@ export class ComfyUI {
 					if (!confirmClear.value || confirm("Clear workflow?")) {
 						app.clean();
 						app.graph.clear();
+						app.resetView();
 					}
 				}
 			}),
 			$el("button", {
 				id: "comfy-load-default-button", textContent: "Load Default", onclick: async () => {
 					if (!confirmClear.value || confirm("Load default workflow?")) {
+						app.resetView();
 						await app.loadGraphData()
 					}
+				}
+			}),
+			$el("button", {
+				id: "comfy-reset-view-button", textContent: "Reset View", onclick: async () => {
+					app.resetView();
 				}
 			}),
 		]);
