aede0b91e0678295b3b89c7679df8bf0e0d73c66
MaraScott <david.asquiedge@marascott.ai>
2024-05-21 23:42:24 +0200

    hotfix access to inputData[1]

--- web/scripts/widgets.js
+++ web/scripts/widgets.js
@@ -348,7 +348,7 @@ export const ComfyWidgets = {
 	STRING(node, inputName, inputData, app) {
 		const defaultVal = inputData[1].default || "";
 		const multiline = !!inputData[1].multiline;
-		inputData[1].placeholder = inputData[1].placeholder || inputData[1].label || inputName;
+		inputData[1].placeholder = inputData[1]?.placeholder || inputData[1]?.label || inputName;
 
 		let res;
 		if (multiline) {
