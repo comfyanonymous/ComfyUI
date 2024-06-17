39d9fc8c5d9fce66d9ee446d44884a8f1beccca5
doctorpangloss <@hiddenswitch.com>
2024-06-17 15:12:17 -0700

    Deprecate main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
+import warnings
 
 from comfy.cmd.main import main
 
 if __name__ == "__main__":
+    warnings.warn("main.py is deprecated. Start comfyui by installing the package through the instructions in the README, not by cloning the repository.", DeprecationWarning)
     asyncio.run(main())
