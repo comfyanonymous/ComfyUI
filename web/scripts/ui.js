1fe15bd47b630744d797bcc303dacc5f61d2fad9
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-06-25 20:56:18 -0400

    Remove duplicated Reset View button

--- web/scripts/ui.js
+++ web/scripts/ui.js
@@ -603,7 +603,6 @@ export class ComfyUI {
 				onclick: () => app.refreshComboInNodes()
 			}),
 			$el("button", {id: "comfy-clipspace-button", textContent: "Clipspace", onclick: () => app.openClipspace()}),
-			$el("button", {id: "comfy-reset-view-button", textContent: "Reset View", onclick: () => app.resetView()}),
 			$el("button", {
 				id: "comfy-clear-button", textContent: "Clear", onclick: () => {
 					if (!confirmClear.value || confirm("Clear workflow?")) {
