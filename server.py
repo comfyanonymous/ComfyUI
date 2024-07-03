63b47537bb784e9fc150abe69581f27818427206
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-07-03 11:15:03 -0400

    Add __module__ to node info
    Use more explicit name 'python_module'
    
    Parse abs ath
    
    Move parse to nodes.py

--- server.py
+++ server.py
@@ -416,6 +416,7 @@ class PromptServer():
             info['name'] = node_class
             info['display_name'] = nodes.NODE_DISPLAY_NAME_MAPPINGS[node_class] if node_class in nodes.NODE_DISPLAY_NAME_MAPPINGS.keys() else node_class
             info['description'] = obj_class.DESCRIPTION if hasattr(obj_class,'DESCRIPTION') else ''
+            info['python_module'] = getattr(obj_class, "RELATIVE_PYTHON_MODULE", "nodes")
             info['category'] = 'sd'
             if hasattr(obj_class, 'OUTPUT_NODE') and obj_class.OUTPUT_NODE == True:
                 info['output_node'] = True
