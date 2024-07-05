7a20b189a5ea82f04b0483ea11ff3ac29d76cddf
doctorpangloss <@hiddenswitch.com>
2024-07-05 13:10:31 -0700

    Actually enable IPEX XPU support

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,7 +54,6 @@ def has_gpu() -> bool:
     try:
         import intel_extension_for_pytorch as ipex
         has_gpu = ipex.xpu.device_count() > 0
-        return has_gpu
     except ImportError:
         try:
             import torch
