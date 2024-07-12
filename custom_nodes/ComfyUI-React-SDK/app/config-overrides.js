181c0197e2d99bbb960e866acaa9b84a00787e32
kim98828 <72718184+kim98828@users.noreply.github.com>
2024-07-12 13:51:59 +0900

    Delete custom_nodes directory

--- custom_nodes/ComfyUI-React-SDK/app/config-overrides.js
+++
@@ -1,8 +0,0 @@
-// module.exports = function override(config, env) {
-//     config.webSocketServer = config.webSocketServer || {}
-//     config.webSocketServer.options = {
-//         ...config.webSocketServer.options,
-//         path: process.env.WDS_SOCKET_PATH,
-//     }
-//     return config
-// }
--- custom_nodes/ComfyUI-React-SDK/app/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
