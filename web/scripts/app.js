d3b32a4dae72eec93b0c35e0fc1b62a1f37616e3
bonlime <bonlimezak@gmail.com>
2024-07-18 13:43:14 +0300

    Sort nodes before saving

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -2033,6 +2033,9 @@ export class ComfyApp {
 		}
 
 		const workflow = graph.serialize();
+		// links are already sorted, but nodes are not
+		workflow["nodes"] = Object.values(workflow["nodes"]).sort((a, b) => a.id - b.id);
+
 		const output = {};
 		// Process nodes in order of execution
 		for (const outerNode of graph.computeExecutionOrder(false)) {
