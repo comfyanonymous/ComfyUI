c79f809b56d5e560402beed0f6dd3277e3f3d85e
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-07-22 11:27:32 -0400

    Add error message on union controlnet (#4081)

--- comfy_extras/nodes_controlnet.py
+++ comfy_extras/nodes_controlnet.py
@@ -1,14 +1,4 @@
-
-UNION_CONTROLNET_TYPES = {"auto": -1,
-                          "openpose": 0,
-                          "depth": 1,
-                          "hed/pidi/scribble/ted": 2,
-                          "canny/lineart/anime_lineart/mlsd": 3,
-                          "normal": 4,
-                          "segment": 5,
-                          "tile": 6,
-                          "repaint": 7,
-                        }
+from comfy.cldm.control_types import UNION_CONTROLNET_TYPES
 
 class SetUnionControlNetType:
     @classmethod
