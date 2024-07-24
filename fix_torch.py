19944ad252106e46a40e0b952c86c1cebc8486ab
comfyanonymous <comfyanonymous@protonmail.com>
2024-07-24 12:49:29 -0400

    Add code to fix issues with new pytorch version on the standalone.

---
+++ fix_torch.py
@@ -0,0 +1,16 @@
+import importlib.util
+import shutil
+import os
+import ctypes
+import logging
+
+
+torch_spec = importlib.util.find_spec("torch")
+for folder in torch_spec.submodule_search_locations:
+    lib_folder = os.path.join(folder, "lib")
+    test_file = os.path.join(lib_folder, "fbgemm.dll")
+    try:
+        mydll = ctypes.cdll.LoadLibrary(test_file)
+    except FileNotFoundError as e:
+        logging.warning("Detected pytorch version with libomp issue, patching.")
+        shutil.copyfile(os.path.join(lib_folder, "libiomp5md.dll"), os.path.join(lib_folder, "libomp140.x86_64.dll"))
