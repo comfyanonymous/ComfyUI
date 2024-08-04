85f74be0f211d9cafa4666671e87d3227468833e
TheUnfocused <33618431+umutpo@users.noreply.github.com>
2024-08-04 13:15:00 -0700

    Fixed a bug

--- run_colab_ubuntu.sh
+++ run_colab_ubuntu.sh
@@ -6,6 +6,7 @@ if ! [ -d "./.venv" ]; then
     python3 -m venv .venv
 fi
 source .venv/bin/activate 
+python3 -m pip install --upgrade pip
 python3 -m pip install pydrive
 sudo python3 connect_to_colab.py
 deactivate
