e7b501f989ead0c04042e8a23908ba6f242512c7
Robin Huang <robin.j.huang@gmail.com>
2024-08-10 13:28:35 -0700

    Add missing type hints.

--- api_server/services/file_service.py
+++ api_server/services/file_service.py
@@ -1,12 +1,13 @@
-from api_server.utils.file_operations import FileSystemOperations
+from typing import Dict, List, Optional
+from api_server.utils.file_operations import FileSystemOperations, FileSystemItem
 
 class FileService:
-    def __init__(self, allowed_directories, file_system_ops=None):
-        self.allowed_directories = allowed_directories
-        self.file_system_ops = file_system_ops or FileSystemOperations()
+    def __init__(self, allowed_directories: Dict[str, str], file_system_ops: Optional[FileSystemOperations] = None):
+        self.allowed_directories: Dict[str, str] = allowed_directories
+        self.file_system_ops: FileSystemOperations = file_system_ops or FileSystemOperations()
 
-    def list_files(self, directory_key):
+    def list_files(self, directory_key: str) -> List[FileSystemItem]:
         if directory_key not in self.allowed_directories:
             raise ValueError("Invalid directory key")
-        directory_path = self.allowed_directories[directory_key]
+        directory_path: str = self.allowed_directories[directory_key]
         return self.file_system_ops.walk_directory(directory_path)
