be66b06cf8ef10ef12df0fd0e8c977b1902c94bb
r42-chun <ChunYou@random42.com>
2024-08-16 13:27:05 +0100

    change save image function to allow output dir specification

--- nodes.py
+++ nodes.py
@@ -1399,9 +1399,13 @@ class SaveImage:
 
     CATEGORY = "image"
 
-    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None, output_dir=None):
+        # ===========================
+        if not output_dir:
+            output_dir = self.output_dir
+        # ===========================
         filename_prefix += self.prefix_append
-        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, output_dir, images[0].shape[1], images[0].shape[0])
         results = list()
         for (batch_number, image) in enumerate(images):
             i = 255. * image.cpu().numpy()
