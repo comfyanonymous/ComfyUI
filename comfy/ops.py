6730f3e1a362d5f3ed44f8541517b03356e7bf0e
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2024-08-18 14:38:09 -0400

    Disable non blocking.
    It fixed some perf issues but caused other issues that need to be debugged.

--- comfy/ops.py
+++ comfy/ops.py
@@ -21,6 +21,7 @@ import comfy.model_management
 
 
 def cast_to(weight, dtype=None, device=None, non_blocking=False):
+    non_blocking = False
     return weight.to(device=device, dtype=dtype, non_blocking=non_blocking)
 
 def cast_to_input(weight, input, non_blocking=False):
