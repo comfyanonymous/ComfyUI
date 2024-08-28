924402d778a57730296a275b4f4974859f15b090
Silver <ishimarukaito@gmail.com>
2024-08-28 08:54:10 +0200

    Remove import from testing phase

--- latent_preview.py
+++ latent_preview.py
@@ -2,7 +2,7 @@ import torch
 from PIL import Image
 import struct
 import numpy as np
-from comfy.cli_args import args, LatentPreviewMethod, LatentPreviewSize
+from comfy.cli_args import args, LatentPreviewMethod
 from comfy.taesd.taesd import TAESD
 import comfy.model_management
 import folder_paths
