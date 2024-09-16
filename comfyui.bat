2836e21a4199af1986d5e8b4f7f17da7bf1af8bd
patientx <patient.x@gmail.com>
2024-09-16 16:20:08 +0300

    Update comfyui.bat

--- comfyui.bat
+++ comfyui.bat
@@ -4,6 +4,7 @@ set PYTHON=%~dp0/venv/Scripts/python.exe
 set GIT=
 set VENV_DIR=./venv
 set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention
+
 set ZLUDA_COMGR_LOG_LEVEL=1
 
 echo *** Checking and updating to new version if possible 
