bb3fc16f1de542fd2682e063525d51c2fe85f253
patientx <patient.x@gmail.com>
2024-09-19 11:59:24 +0300

    Update fixforrx580.bat

--- fixforrx580.bat
+++ fixforrx580.bat
@@ -8,7 +8,7 @@ echo .....................................................
 echo *** "Patching ZLUDA Again"
 echo.
 rmdir /S /q zluda
-curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.11cc5844514f93161e0e74387f04e2c537705a82/ZLUDA-windows-amd64.zip > zluda.zip
+curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 tar -xf zluda.zip
 del zluda.zip
 copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
