d3df22f4e31acde1d564fb0a65cf6c3d2a38e74c
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-09-22 12:04:24 +0900

    Update web content to release v1.2.60

---
+++ web/extensions/core/colorPalette.js
@@ -0,0 +1,3 @@
+// Shim for extensions/core/colorPalette.ts
+export const defaultColorPalette = window.comfyAPI.colorPalette.defaultColorPalette;
+export const getColorPalette = window.comfyAPI.colorPalette.getColorPalette;
