ab1a1de7a44dcd2463ae22b41e7b182d4186e3f5
doctorpangloss <@hiddenswitch.com>
2024-09-26 13:26:52 -0700

    Fix missing arg to add_model_folder_path

--- comfy/extra_config.py
+++ comfy/extra_config.py
@@ -28,4 +28,4 @@ def load_extra_path_config(yaml_path):
                 if base_path is not None:
                     full_path = os.path.join(base_path, full_path)
                 logging.info("Adding extra search path {} {}".format(x, full_path))
-                folder_paths.add_model_folder_path(x, full_path, is_default)
+                folder_paths.add_model_folder_path(x, full_path, is_default=is_default)
