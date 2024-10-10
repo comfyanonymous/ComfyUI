14eba07acd28d5b0fc250e493ab7751bd9b9b43e
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-10-09 22:37:04 -0400

    Update web content to release v1.3.11 (#5189)
    * Update web content to release v1.3.11
    
    * nit

--- web/scripts/changeTracker.js
+++ web/scripts/changeTracker.js
@@ -1,2 +1,3 @@
 // Shim for scripts/changeTracker.ts
 export const ChangeTracker = window.comfyAPI.changeTracker.ChangeTracker;
+export const globalTracker = window.comfyAPI.changeTracker.globalTracker;
