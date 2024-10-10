aa6790c2032f290e0e31bcaf5d9f8ff6c732eab0
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-10-09 22:37:04 -0400

    Update web content to release v1.3.11 (#5189)
    * Update web content to release v1.3.11
    
    * nit

--- web/extensions/core/widgetInputs.js
+++ web/extensions/core/widgetInputs.js
@@ -1,4 +1,5 @@
 // Shim for extensions/core/widgetInputs.ts
 export const getWidgetConfig = window.comfyAPI.widgetInputs.getWidgetConfig;
+export const convertToInput = window.comfyAPI.widgetInputs.convertToInput;
 export const setWidgetConfig = window.comfyAPI.widgetInputs.setWidgetConfig;
 export const mergeIfValid = window.comfyAPI.widgetInputs.mergeIfValid;
