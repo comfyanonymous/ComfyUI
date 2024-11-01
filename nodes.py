9cfd46b2d9a21c54ebbf88fbd435ec1cd3881315
traugdor <traugdor@users.noreply.github.com>
2024-11-01 16:19:30 -0500

    Update nodes.py
    Remove unnecessary parameter to decode method

--- nodes.py
+++ nodes.py
@@ -280,7 +280,7 @@ class VAEDecode:
     CATEGORY = "latent"
     DESCRIPTION = "Decodes latent images back into pixel space images."
 
-    def decode(self, vae, samples, tile_size=512):
+    def decode(self, vae, samples):
         images = vae.decode(samples["samples"])
         if len(images.shape) == 5: #Combine batches
             images = images.reshape(-1, images.shape[-3], images.shape[-2], images.shape[-1])
