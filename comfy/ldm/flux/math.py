ef2d5933f54bafb139b23054d994625208667763
Raphael Walker <slickytail.mc@gmail.com>
2024-12-10 11:43:53 +0100

    mask with image_ref_size

--- comfy/ldm/flux/math.py
+++ comfy/ldm/flux/math.py
@@ -1,6 +1,8 @@
 import torch
 from einops import rearrange
 from torch import Tensor
+from torch.nn.functional import interpolate
+
 from comfy.ldm.modules.attention import optimized_attention
 import comfy.model_management
 
@@ -33,3 +35,4 @@ def apply_rope(xq: Tensor, xk: Tensor, freqs_cis: Tensor):
     xq_out = freqs_cis[..., 0] * xq_[..., 0] + freqs_cis[..., 1] * xq_[..., 1]
     xk_out = freqs_cis[..., 0] * xk_[..., 0] + freqs_cis[..., 1] * xk_[..., 1]
     return xq_out.reshape(*xq.shape).type_as(xq), xk_out.reshape(*xk.shape).type_as(xk)
+
