144255a06d6d8335a2522b959b16bc761e5fd5a0
yoinked <pepe.dannyboy@gmail.com>
2024-12-10 08:23:11 -0800

    revert the sampler_cfg_function thing

--- comfy_extras/nodes_mahiro.py
+++ comfy_extras/nodes_mahiro.py
@@ -28,8 +28,8 @@ class Mahiro:
             sim = F.cosine_similarity(normu, normm).mean()
             simsc = 2 * (sim+1)
             wm = (simsc*cfg + (4-simsc)*leap) / 4
-            return args['input'] - wm
-        m.set_model_sampler_cfg_function(mahiro_normd)
+            return wm
+        m.set_model_sampler_post_cfg_function(mahiro_normd)
         return (m, )
 
 NODE_CLASS_MAPPINGS = {
