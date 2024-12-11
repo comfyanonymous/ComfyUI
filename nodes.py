5bea1d2ec95ca4519ed96b09b45fcd327e6dc751
yoinked <pepe.dannyboy@gmail.com>
2024-12-11 13:51:51 -0800

    Add MaHiRo (improved/alternate CFG) (#5975)
    * Add MaHiRo (improved CFG)
    
    long explanation of what it is is [here](https://huggingface.co/spaces/yoinked/blue-arxiv) (2024-1208.1) 
    
    
    note: if the node name has encoding issues (utf 8/whatever), id suggest to replace the face at the end with `(>w<)`
    
    * add it to nodes.py, add description, and make it a post_cfg function
    
    * fix
    
    * revert the sampler_cfg_function thing
    
    * switch cfg to args["denoised"]

--- nodes.py
+++ nodes.py
@@ -2147,6 +2147,7 @@ def init_builtin_extra_nodes():
         "nodes_torch_compile.py",
         "nodes_mochi.py",
         "nodes_slg.py",
+        "nodes_mahiro.py",
         "nodes_lt.py",
         "nodes_hooks.py",
     ]
