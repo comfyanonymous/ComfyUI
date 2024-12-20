b5e72b093b1c236539782fc0278c20c3778d1db8
patientx <patient.x@gmail.com>
2024-12-20 10:54:05 +0300

    added reserve-vram for better memory management for low mem amd gpu's.

--- comfyui.bat
+++ comfyui.bat
@@ -3,7 +3,7 @@
 set PYTHON="%~dp0/venv/Scripts/python.exe"
 set GIT=
 set VENV_DIR=./venv
-set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention
+set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
 
 set ZLUDA_COMGR_LOG_LEVEL=1
 
