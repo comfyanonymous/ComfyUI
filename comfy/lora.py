ceee378d86b6f40a4856fb3e7c294f9301bc0e09
rickard <rickard@mindemia.com>
2024-12-25 18:04:56 +0100

    replace 'diffusion_model' with 'transformer'

--- comfy/lora.py
+++ comfy/lora.py
@@ -401,9 +401,9 @@ def model_lora_keys_unet(model, key_map={}):
 
     if isinstance(model, comfy.model_base.LTXV):
         for k in sdk:
-            if k.startswith("transformer.") and k.endswith(".weight"): #Official Mochi lora format
-                key_lora = k[len("transformer."):-len(".weight")]
-                key_map["{}".format(key_lora)] = k
+            if k.startswith("diffusion_model.") and k.endswith(".weight"):
+                key_lora = k[len("diffusion_model."):-len(".weight")]
+                key_map["transformer.{}".format(key_lora)] = k
 
     return key_map
 
