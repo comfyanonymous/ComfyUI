466905b1951fec1c69e3e0bdfad4ea4fd7d54eb9
patientx <patient.x@gmail.com>
2024-12-30 10:27:16 +0300

    Update comfyui.bat

--- comfyui.bat
+++ comfyui.bat
@@ -4,7 +4,7 @@ set PYTHON="%~dp0/venv/Scripts/python.exe"
 set GIT=
 set VENV_DIR=./venv
 
-set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:512
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.8,max_split_size_mb:512
 
 set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
 
