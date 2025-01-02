8bad50e609fec2b780963db7672fe3ccbb5d6c92
huchenlei <huchenlei@proton.me>
2025-01-01 19:50:29 -0500

    Update web content to release v1.6.14

--- web/scripts/widgets.js
+++ web/scripts/widgets.js
@@ -1,6 +1,6 @@
 // Shim for scripts/widgets.ts
 export const updateControlWidgetLabel = window.comfyAPI.widgets.updateControlWidgetLabel;
+export const IS_CONTROL_WIDGET = window.comfyAPI.widgets.IS_CONTROL_WIDGET;
 export const addValueControlWidget = window.comfyAPI.widgets.addValueControlWidget;
 export const addValueControlWidgets = window.comfyAPI.widgets.addValueControlWidgets;
-export const initWidgets = window.comfyAPI.widgets.initWidgets;
 export const ComfyWidgets = window.comfyAPI.widgets.ComfyWidgets;
