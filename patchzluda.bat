0bc68eca5247f8cbaea0b04f05173db7184bd6d9
patientx <patient.x@gmail.com>
2025-01-07 16:49:33 +0300

    updated zluda version to 3.8.5

--- patchzluda.bat
+++ patchzluda.bat
@@ -1,10 +1,10 @@
 @Echo off
 rmdir /S /q zluda
-curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
+curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.2930436a356baabafad9e66d49a5929ad2fc3eb9/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 tar -xf zluda.zip
 del zluda.zip
 copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
 copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y
 copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y
 echo.
-@echo * ZLUDA is patched. (Zluda 3.8.4 for HIP SDK 5.7)
+@echo * ZLUDA is patched. (Zluda 3.8.5 for HIP SDK 5.7.1)
