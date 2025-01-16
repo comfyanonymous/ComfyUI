88ceb28e2094e97d837ea86338121ea8f9f7d38e
comfyanonymous <comfyanonymous@protonmail.com>
2025-01-16 06:31:03 -0500

    Tweak hunyuan memory usage factor.

--- comfy/supported_models.py
+++ comfy/supported_models.py
@@ -788,7 +788,7 @@ class HunyuanVideo(supported_models_base.BASE):
     unet_extra_config = {}
     latent_format = latent_formats.HunyuanVideo
 
-    memory_usage_factor = 1.7 #TODO
+    memory_usage_factor = 1.8 #TODO
 
     supported_inference_dtypes = [torch.bfloat16, torch.float32]
 
