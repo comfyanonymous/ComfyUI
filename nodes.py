095f03683914d4e95b279a9242e7ae7fbee39f5d
comfyanonymous <comfyanonymous@protonmail.com>
2025-01-26 06:04:57 -0500

    Better CLIPTextEncode error when clip input is None.

--- nodes.py
+++ nodes.py
@@ -63,6 +63,8 @@ class CLIPTextEncode(ComfyNodeABC):
     DESCRIPTION = "Encodes a text prompt using a CLIP model into an embedding that can be used to guide the diffusion model towards generating specific images."
 
     def encode(self, clip, text):
+        if clip is None:
+            raise RuntimeError("ERROR: clip input is invalid: None\n\nIf the clip is from a checkpoint loader node your checkpoint does not contain a valid clip or text encoder model.")
         tokens = clip.tokenize(text)
         return (clip.encode_from_tokens_scheduled(tokens), )
 
