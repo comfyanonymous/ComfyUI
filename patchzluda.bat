1f5708663a85980920e64c810735abf2a4435b78
patientx <patient.x@gmail.com>
2025-02-07 12:48:02 +0300

    Update patchzluda.bat

--- patchzluda.bat
+++ patchzluda.bat
@@ -1,6 +1,6 @@
 @Echo off
 rmdir /S /q zluda
-curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
+curl -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 tar -xf zluda.zip
 del zluda.zip
 copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
