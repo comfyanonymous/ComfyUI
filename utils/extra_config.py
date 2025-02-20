29d4384a75abb4ca90f5b64e70499f2120d76a3a
Robin Huang <robin.j.huang@gmail.com>
2025-02-20 04:09:45 -0800

    Normalize extra_model_config.yaml paths to prevent duplicates. (#6885)
    * Normalize extra_model_config.yaml paths before adding.
    
    * Fix tests.
    
    * Fix tests.

--- utils/extra_config.py
+++ utils/extra_config.py
@@ -29,5 +29,6 @@ def load_extra_path_config(yaml_path):
                     full_path = os.path.join(base_path, full_path)
                 elif not os.path.isabs(full_path):
                     full_path = os.path.abspath(os.path.join(yaml_dir, y))
-                logging.info("Adding extra search path {} {}".format(x, full_path))
-                folder_paths.add_model_folder_path(x, full_path, is_default)
+                normalized_path = os.path.normpath(full_path)
+                logging.info("Adding extra search path {} {}".format(x, normalized_path))
+                folder_paths.add_model_folder_path(x, normalized_path, is_default)
