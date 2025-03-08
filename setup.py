0fbce0a28299384e4f94ab4ff301f7a566dc3461
doctorpangloss <@hiddenswitch.com>
2025-03-08 15:55:12 -0800

    Fix installation of frontend package

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ def _is_linux_arm64():
 def dependencies(install_torch_for_system=False, force_nightly: bool = False) -> List[str]:
     _dependencies = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
     if not install_torch_for_system:
-        return [dep for dep in _dependencies if "@" not in dep]
+        return [dep for dep in _dependencies if "@" not in dep or "comfyui-frontend-package" in dep]
     # If we're installing with no build isolation, we can check if torch is already installed in the environment, and if
     # so, go ahead and use the version that is already installed.
     existing_torch: Optional[str]
