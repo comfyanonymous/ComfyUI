10310baf3fff93500ee2efdcab79e79692770e72
Jacob Segal <jacob.e.segal@gmail.com>
2025-04-28 19:37:27 -0700

    Fix linting issue

--- comfy_api/input/video_types.py
+++ comfy_api/input/video_types.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Optional
-from comfy_api.util import VideoContainer, VideoCodec
+from comfy_api.util import VideoContainer, VideoCodec, VideoComponents
 
 class VideoInput(ABC):
     """
