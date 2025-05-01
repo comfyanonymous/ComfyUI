d136063e8fb1d967ffa0cf93064bdc50381d7c3c
Christian Byrne <cbyrne@comfy.org>
2025-04-30 22:04:33 -0700

    Fix: typo in node name "Stabiliy" => "Stability" (#91)

--- comfy_api_nodes/nodes_stability.py
+++ comfy_api_nodes/nodes_stability.py
@@ -156,5 +156,5 @@ NODE_CLASS_MAPPINGS = {
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "StabilityStableImageUltraNode": "Stabiliy Stable Image Ultra",
+    "StabilityStableImageUltraNode": "Stability Stable Image Ultra",
 }
