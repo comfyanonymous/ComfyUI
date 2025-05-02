996191d993bb0bbb3fcb04e4c5492f28c253bf20
patientx <patient.x@gmail.com>
2025-05-02 18:17:46 +0300

    Add files via upload

---
+++ install-n.bat
@@ -0,0 +1,80 @@
+@echo off
+
+title ComfyUI-Zluda Installer
+
+set ZLUDA_COMGR_LOG_LEVEL=1
+setlocal EnableDelayedExpansion
+set "startTime=%time: =0%"
+
+cls
+echo ---------------------------------------------------------------
+Echo * COMFYUI-ZLUDA INSTALL (for HIP 6.2.4 with MIOPEN and Triton)*
+echo ---------------------------------------------------------------
+echo.
+echo  ::  %time:~0,8%  ::  - Setting up the virtual enviroment
+Set "VIRTUAL_ENV=venv"
+If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
+    python.exe -m venv %VIRTUAL_ENV%
+)
+
+If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" Exit /B 1
+
+echo  ::  %time:~0,8%  ::  - Virtual enviroment activation
+Call "%VIRTUAL_ENV%\Scripts\activate.bat"
+echo  ::  %time:~0,8%  ::  - Updating the pip package 
+python.exe -m pip install --upgrade pip --quiet
+echo.
+echo  ::  %time:~0,8%  ::  Beginning installation ...
+echo.
+echo  ::  %time:~0,8%  ::  - Installing required packages
+pip install -r requirements.txt --quiet
+echo  ::  %time:~0,8%  ::  - Installing torch for AMD GPUs (First file is 2.7 GB, please be patient)
+pip uninstall torch torchvision torchaudio -y --quiet
+pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu118 --quiet
+echo  ::  %time:~0,8%  ::  - Installing onnxruntime (required by some nodes)
+pip install onnxruntime --quiet
+echo  ::  %time:~0,8%  ::  - (temporary numpy fix)
+pip uninstall numpy -y --quiet
+pip install numpy==1.26.0 --quiet
+echo.
+echo  ::  %time:~0,8%  ::  Custom node(s) installation ...
+echo. 
+echo  ::  %time:~0,8%  ::  - Installing Comfyui Manager
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git --quiet
+echo  ::  %time:~0,8%  ::  - Installing ComfyUI-deepcache
+git clone https://github.com/styler00dollar/ComfyUI-deepcache.git --quiet
+cd ..
+echo. 
+echo  ::  %time:~0,8%  ::  - Patching ZLUDA
+
+:: Download matching ZLUDA version
+rmdir /S /Q zluda 2>nul
+mkdir zluda
+cd zluda
+%SystemRoot%\system32\curl.exe -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.0d1513a017397bf9ebbac0b3c846160c8d4fc700/ZLUDA-nightly-windows-rocm6-amd64.zip > zluda.zip
+%SystemRoot%\system32\tar.exe -xf zluda.zip
+del zluda.zip
+cd ..
+
+:: Patch DLLs
+copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y >NUL
+copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y >NUL
+copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y >NUL
+copy zluda\cudnn.dll venv\Lib\site-packages\torch\lib\cudnn64_9.dll /y >NUL
+copy comfy\customzluda\zluda.py comfy\zluda.py /y >NUL
+
+echo  ::  %time:~0,8%  ::  - ZLUDA patched for HIP SDK 6.2.4 with miopen and triton-flash attention.
+echo. 
+set "endTime=%time: =0%"
+set "end=!endTime:%time:~8,1%=%%100)*100+1!"  &  set "start=!startTime:%time:~8,1%=%%100)*100+1!"
+set /A "elap=((((10!end:%time:~2,1%=%%100)*60+1!%%100)-((((10!start:%time:~2,1%=%%100)*60+1!%%100), elap-=(elap>>31)*24*60*60*100"
+set /A "cc=elap%%100+100,elap/=100,ss=elap%%60+100,elap/=60,mm=elap%%60+100,hh=elap/60+100"
+echo ..................................................... 
+echo *** Installation is completed in %hh:~1%%time:~2,1%%mm:~1%%time:~2,1%%ss:~1%%time:~8,1%%cc:~1% . 
+echo *** echo *** You can use "comfyui-n.bat" to start the app later. 
+echo ..................................................... 
+echo.
+echo *** Starting the Comfyui-ZLUDA for the first time, please be patient...
+echo.
+.\zluda\zluda.exe -- python main.py --auto-launch --use-pytorch-cross-attention
