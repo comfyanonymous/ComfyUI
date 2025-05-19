1bd5058d0fc22f8eba8abc390ff62ad575f4be0d
benceruleanlu <benceruleanlu@proton.me>
2025-05-19 00:05:24 -0400

    Support linking MD files

--- comfy/comfy_types/node_typing.py
+++ comfy/comfy_types/node_typing.py
@@ -237,7 +237,7 @@ class ComfyNodeABC(ABC):
     API_NODE: Optional[bool]
     """Flags a node as an API node. See: https://docs.comfy.org/tutorials/api-nodes/overview."""
     HELP: Optional[str]
-    """The markdown help text for the node. Used to inform users about the node in general."""
+    """The help markdown or path to the help markdown file for the node. Used to inform users about the node in general."""
 
     @classmethod
     @abstractmethod
