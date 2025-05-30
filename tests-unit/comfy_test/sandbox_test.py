3774f2bb8e0d7d4556e9d30a45e1b15f62b890b9
Deepanjan Roy <deepanjan7@gmail.com>
2025-05-30 18:36:57 -0400

    Clean up

--- tests-unit/comfy_test/sandbox_test.py
+++ tests-unit/comfy_test/sandbox_test.py
@@ -5,32 +5,31 @@ def test_icacl_no_low_integrity_label():
     icacl_output = r"""
     foo NT AUTHORITY\SYSTEM:(OI)(CI)(F)
     """
-    assert not windows_sandbox.permits_low_integrity_write(icacl_output)
+    assert not windows_sandbox.does_permit_low_integrity_write(icacl_output)
 
 def test_icacl_missing_inherit_flags():
     icacl_output = r"""
     foo Mandatory Label\Low Mandatory Level:(NW)
     """
-    assert not windows_sandbox.permits_low_integrity_write(icacl_output)
+    assert not windows_sandbox.does_permit_low_integrity_write(icacl_output)
 
     icacl_output = r"""
     foo Mandatory Label\Low Mandatory Level:(OI)(NW)
     """
-    assert not windows_sandbox.permits_low_integrity_write(icacl_output)
+    assert not windows_sandbox.does_permit_low_integrity_write(icacl_output)
 
     icacl_output = r"""
     foo Mandatory Label\Low Mandatory Level:(CI)(NW)
     """
-    assert not windows_sandbox.permits_low_integrity_write(icacl_output)
+    assert not windows_sandbox.does_permit_low_integrity_write(icacl_output)
 
 def test_icacl_correct_acls():
     icacl_output = r"""
     foo Mandatory Label\Low Mandatory Level:(I)(OI)(CI)(NW)
     """
-    assert windows_sandbox.permits_low_integrity_write(icacl_output)
+    assert windows_sandbox.does_permit_low_integrity_write(icacl_output)
 
     icacl_output = r"""
     foo Mandatory Label\Low Mandatory Level:(OI)(CI)(NW)
     """
-    assert windows_sandbox.permits_low_integrity_write(icacl_output)
-    
+    assert windows_sandbox.does_permit_low_integrity_write(icacl_output)
