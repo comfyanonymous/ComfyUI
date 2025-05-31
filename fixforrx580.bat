ac081c7ac3646adeeb991e5799c005cbcce16888
patientx <patient.x@gmail.com>
2025-05-31 16:58:49 +0300

    Update fixforrx580.bat

--- fixforrx580.bat
+++ fixforrx580.bat
@@ -19,9 +19,10 @@ rmdir /S /q zluda
 %SystemRoot%\system32\curl.exe -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 %SystemRoot%\system32\tar.exe -xf zluda.zip
 del zluda.zip
-copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
-copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y
-copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y
+copy zluda\cublas.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cublas64_11.dll /y >NUL
+copy zluda\cusparse.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cusparse64_11.dll /y >NUL
+copy %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc_cuda.dll /y >NUL
+copy zluda\nvrtc.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y >NUL
 echo.
 @echo  *ZLUDA is patched. *
 echo.
