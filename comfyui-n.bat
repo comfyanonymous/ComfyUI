811ee8a18524485756b4d6b02b2a83eb8df3e0d2
patientx <patient.x@gmail.com>
2025-06-12 16:17:10 +0300

    Update comfyui-n.bat

--- comfyui-n.bat
+++ comfyui-n.bat
@@ -10,12 +10,16 @@ set PYTHON="%~dp0/venv/Scripts/python.exe"
 set GIT=
 set VENV_DIR=./venv
 
-set COMMANDLINE_ARGS=--auto-launch --use-pytorch-cross-attention --reserve-vram 0.9
+set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
 
 set ZLUDA_COMGR_LOG_LEVEL=1
 
 echo *** Checking and updating to new version if possible 
+
+copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
 git pull
+copy comfy\customzluda\zluda.py comfy\zluda.py /y >NUL
+
 echo.
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
 pause
