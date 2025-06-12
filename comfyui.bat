273d40dc03d767e5d7fdf4a4a462d685b64cfb50
patientx <patient.x@gmail.com>
2025-06-12 16:16:09 +0300

    update won't need zluda.py to be deleted anymore

--- comfyui.bat
+++ comfyui.bat
@@ -9,7 +9,11 @@ set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
 set ZLUDA_COMGR_LOG_LEVEL=1
 
 echo *** Checking and updating to new version if possible 
+
+copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
 git pull
+copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
+
 echo.
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
 pause
