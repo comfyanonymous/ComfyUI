e8cf6489d2f12afa0e29178f6a3cff5917efded8
doctorpangloss <@hiddenswitch.com>
2025-06-17 10:39:51 -0700

    modify hook breaker to work

--- comfy/cmd/hook_breaker_ac10a0.py
+++ comfy/cmd/hook_breaker_ac10a0.py
@@ -1,7 +1,7 @@
 # Prevent custom nodes from hooking anything important
-import comfy.model_management
+from .. import model_management
 
-HOOK_BREAK = [(comfy.model_management, "cast_to")]
+HOOK_BREAK = [(model_management, "cast_to")]
 
 
 SAVED_FUNCTIONS = []
