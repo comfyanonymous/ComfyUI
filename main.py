1883e70b4374a3317e0463a0bef292fc21182bad
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2025-06-21 00:30:39 -0700

    Fix exception when using a noise mask with cosmos predict2. (#8621)
    * Fix exception when using a noise mask with cosmos predict2.
    
    * Fix ruff.

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ def prompt_worker(q, server_instance):
 
             current_time = time.perf_counter()
             execution_time = current_time - execution_start_time
-            
+
             # Log Time in a more readable way after 10 minutes
             if execution_time > 600:
                 execution_time = time.strftime("%H:%M:%S", time.gmtime(execution_time))
