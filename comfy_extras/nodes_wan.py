8d15d9f77efe8aba557feb4733f0243354808519
Eugene Fairley <ecfairley@ucdavis.edu>
2025-06-29 12:21:15 -0700

    'save'

--- comfy_extras/nodes_wan.py
+++ comfy_extras/nodes_wan.py
@@ -637,7 +637,7 @@ class WanTrackToVideo:
             print(f"Processed tracks: {processed_tracks}")  # Debugging line
             
             if start_image is not None:
-                start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "lanczos", "disabled").movedim(1, -1)
+                start_image = comfy.utils.common_upscale(start_image[:length].movedim(-1, 1), width, height, "bilinear", "center").movedim(1, -1)
                 start_image = start_image * 2 - 1
                 lat_h = height // 8
                 lat_w = width // 8
