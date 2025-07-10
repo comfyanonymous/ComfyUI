8b842a9efc503fcfe6c21453130841ce50a6a9f5
patientx <patient.x@gmail.com>
2025-07-10 23:54:16 +0300

    Update fixforrx580.bat

--- fixforrx580.bat
+++ fixforrx580.bat
@@ -16,7 +16,7 @@ echo .....................................................
 echo *** "Patching ZLUDA Again"
 echo.
 rmdir /S /q zluda
-%SystemRoot%\system32\curl.exe -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
+%SystemRoot%\system32\curl.exe -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.5e717459179dc272b7d7d23391f0fad66c7459cf/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 %SystemRoot%\system32\tar.exe -xf zluda.zip
 del zluda.zip
 copy zluda\cublas.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cublas64_11.dll /y >NUL
@@ -24,7 +24,7 @@ copy zluda\cusparse.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cusparse64_11.
 copy %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc_cuda.dll /y >NUL
 copy zluda\nvrtc.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y >NUL
 echo.
-@echo  *ZLUDA is patched. *
+@echo  *ZLUDA 3.9.5 for hip 5.7.1 is patched. *
 echo.
 echo ..................................................... 
 echo *** Installation is done. --
