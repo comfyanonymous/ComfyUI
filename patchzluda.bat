646716133b9588b31376fdc460ac812a55a279ad
patientx <patient.x@gmail.com>
2025-07-10 23:48:08 +0300

    Update patchzluda.bat

--- patchzluda.bat
+++ patchzluda.bat
@@ -1,10 +1,10 @@
 @Echo off
 rmdir /S /q zluda
-%SystemRoot%\system32\curl.exe -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.c0804ca624963aab420cb418412b1c7fbae3454b/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
+%SystemRoot%\system32\curl.exe -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.5e717459179dc272b7d7d23391f0fad66c7459cf/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 %SystemRoot%\system32\tar.exe -xf zluda.zip
 del zluda.zip
 copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
 copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y
 copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y
 echo.
-echo * ZLUDA is patched. (Zluda 3.8.4 for HIP SDK 5.7.1)
+echo * ZLUDA is patched. (Zluda 3.9.5 for HIP SDK 5.7.1)
