4a461b6093502a717e77571d712f8a5e84127cb2
Jacob Segal <jacob.e.segal@gmail.com>
2025-07-22 18:35:02 -0700

    Fix missing backward compatibility proxy

---
+++ comfy_api/util/__init__.py
@@ -0,0 +1,2 @@
+# This file only exists for backwards compatibility.
+from comfy_api.latest.util import *  # noqa: F403
---
+++ comfy_api/util/video_types.py
@@ -0,0 +1,2 @@
+# This file only exists for backwards compatibility.
+from comfy_api.latest.util.video_types import *  # noqa: F403
