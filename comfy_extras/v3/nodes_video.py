00c46797b8caffde86b9403f8ed3dc5d8ef25b85
Jedrzej Kosinski <kosinkadink1@gmail.com>
2025-07-24 18:32:18 -0700

    Satisfy ruff by sorting imports

--- comfy_extras/v3/nodes_video.py
+++ comfy_extras/v3/nodes_video.py
@@ -11,8 +11,8 @@ import folder_paths
 from comfy.cli_args import args
 from comfy_api.input import AudioInput, ImageInput, VideoInput
 from comfy_api.input_impl import VideoFromComponents, VideoFromFile
-from comfy_api.util import VideoCodec, VideoComponents, VideoContainer
 from comfy_api.latest import io, ui
+from comfy_api.util import VideoCodec, VideoComponents, VideoContainer
 
 
 class CreateVideo(io.ComfyNode):
