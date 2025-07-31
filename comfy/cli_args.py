eb84d6095fe62b4e22c663a0def686689b5dce8e
bilt <vanderbilt@gmail.com>
2025-07-31 15:30:25 +0800

    fix

--- comfy/cli_args.py
+++ comfy/cli_args.py
@@ -49,6 +49,9 @@ parser.add_argument("--temp-directory", type=str, default=None, help="Set the Co
 parser.add_argument("--input-directory", type=str, default=None, help="Set the ComfyUI input directory. Overrides --base-directory.")
 parser.add_argument("--auto-launch", action="store_true", help="Automatically launch ComfyUI in the default browser.")
 parser.add_argument("--disable-auto-launch", action="store_true", help="Disable auto launching the browser.")
+parser.add_argument("--disable_trigger_vercel_deploy", action="store_true", help="Disable vercel deploy.")
+parser.add_argument("--frp_remote_port_idx", type=int, default=None, help="Set remote port idx.")
+
 parser.add_argument("--cuda-device", type=int, default=None, metavar="DEVICE_ID", help="Set the id of the cuda device this instance will use.")
 cm_group = parser.add_mutually_exclusive_group()
 cm_group.add_argument("--cuda-malloc", action="store_true", help="Enable cudaMallocAsync (enabled by default for torch 2.0 and up).")
