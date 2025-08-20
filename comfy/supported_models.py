0f584ea9835ff97d9263e2e88d9c635f918af16c
blepping <blepping@tuta.io>
2025-08-20 12:30:27 -0600

    Increase Chroma Radiance memory usage factor once again

--- comfy/supported_models.py
+++ comfy/supported_models.py
@@ -1213,7 +1213,7 @@ class ChromaRadiance(Chroma):
     latent_format = comfy.latent_formats.ChromaRadiance
 
     # Pixel-space model, no spatial compression for model input.
-    memory_usage_factor = 0.55
+    memory_usage_factor = 0.75
 
     def get_model(self, state_dict, prefix="", device=None):
         return model_base.ChromaRadiance(self, device=device)
