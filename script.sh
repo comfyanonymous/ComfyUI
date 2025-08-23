ae413867c287fa968770276e7b0f3cad7e90151d
bilt <vanderbilt@gmail.com>
2025-08-23 11:27:12 +0800

    ComfyUI-PuLID-Flux-Enhanced

--- script.sh
+++ script.sh
@@ -340,11 +340,11 @@ cd PuLID_ComfyUI
 pip install -r requirements.txt
 cd /kaggle/ComfyUI
 
-# cd custom_nodes
-# git clone https://github.com/sipie800/ComfyUI-PuLID-Flux-Enhanced.git
-# cd ComfyUI-PuLID-Flux-Enhanced
-# pip install -r requirements.txt
-# cd /kaggle/ComfyUI
+cd custom_nodes
+git clone https://github.com/sipie800/ComfyUI-PuLID-Flux-Enhanced.git
+cd ComfyUI-PuLID-Flux-Enhanced
+pip install -r requirements.txt
+cd /kaggle/ComfyUI
 
 cd custom_nodes
 git clone https://github.com/balazik/ComfyUI-PuLID-Flux.git
