4ac02dcde8f3e9964b6479021efa154d876fcb72
patientx <patient.x@gmail.com>
2025-08-24 01:44:16 +0300

    Update patchzluda.bat

--- patchzluda.bat
+++ patchzluda.bat
@@ -1,5 +1,17 @@
-@Echo off
-rmdir /S /q zluda
+cls
+echo -----------------------------------------------------------------------
+Echo                   * ZLUDA Patcher (for HIP 5.7.1)*
+echo -----------------------------------------------------------------------
+echo.
+echo :: Activating virtual environment
+Call "venv\Scripts\activate.bat"
+echo :: Uninstalling previous torch packages
+pip uninstall torch torchvision torchaudio -y --quiet
+rmdir /S /Q "venv\Lib\site-packages\torch" 2>nul
+echo :: Installing torch 2.3 - torchaudio 2.3 and torchvision 0.18
+pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118 --quiet
+pip install numpy==1.26.4 --quiet
+rmdir /S /Q zluda 2>nul
 %SystemRoot%\system32\curl.exe -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.5e717459179dc272b7d7d23391f0fad66c7459cf/ZLUDA-windows-rocm5-amd64.zip > zluda.zip
 %SystemRoot%\system32\tar.exe -xf zluda.zip
 del zluda.zip
