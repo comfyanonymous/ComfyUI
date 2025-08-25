9c3295ca6d24c9dd81efecc2e7e21f43d23dddd9
bilt <vanderbilt@gmail.com>
2025-08-25 15:35:40 +0800

    ComfyUI_roop

--- script.sh
+++ script.sh
@@ -389,6 +389,12 @@ cd custom_nodes
 git clone https://github.com/tusharbhutt/Endless-Nodes.git
 cd /kaggle/ComfyUI
 
+cd custom_nodes
+git clone https://github.com/ssitu/ComfyUI_roop
+cd ComfyUI_roop
+pip install -r requirements.txt
+cd /kaggle/ComfyUI
+
 # REMOTE_PORT="$1"
 
 # 传入参数3,4,5,...  这样便于扩展，如果以后用了其他的frp，也好调整。
