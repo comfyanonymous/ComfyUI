fa2bac8a013a75f4b996aa89826a79f78aad4704
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2025-08-29 20:06:04 -0700

    Lower ram usage on windows. (#9628)

--- main.py
+++ main.py
@@ -112,6 +112,7 @@ import gc
 
 
 if os.name == "nt":
+    os.environ['MIMALLOC_PURGE_DELAY'] = '0'
     logging.getLogger("xformers").addFilter(lambda record: 'A matching Triton is not available' not in record.getMessage())
 
 if __name__ == "__main__":
