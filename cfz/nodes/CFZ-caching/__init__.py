73774a5fac46a05894a8b344740e2fbfb3b1c438
patientx <patient.x@gmail.com>
2025-09-05 17:13:02 +0300

    Update __init__.py

--- cfz/nodes/CFZ-caching/__init__.py
+++ cfz/nodes/CFZ-caching/__init__.py
@@ -1,12 +1,13 @@
-from .cfz_caching_condition import save_conditioning, load_conditioning
+from .cfz_caching_condition import save_conditioning, load_conditioning, CFZ_PrintMarker
 
-# CFZ Save-Load Conditioning Node Pack
 NODE_CLASS_MAPPINGS = {
     "CFZ_save_conditioning": save_conditioning,
     "CFZ_load_conditioning": load_conditioning,
+    "CFZ_PrintMarker": CFZ_PrintMarker,   # NEW
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CFZ_save_conditioning": "CFZ Save Conditioning",
     "CFZ_load_conditioning": "CFZ Load Conditioning",
+    "CFZ_PrintMarker": "CFZ Print Marker",
 }
