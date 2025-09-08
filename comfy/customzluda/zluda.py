40199a5244f0c2c9b45b9aa19c978bee8795d471
Rando717 <respect_killer@outlook.com>
2025-09-08 17:00:40 +0200

    Update zluda.py (print Triton version)
    Added check for Triton version string, if it exists.
    Could be useful info for troubleshooting reports.

--- comfy/customzluda/zluda.py
+++ comfy/customzluda/zluda.py
@@ -431,6 +431,13 @@ try:
     import triton.language as tl
     print("  ::  Triton core imported successfully")
 
+    # Get triton version, if possible, continue if no version string
+    try:
+        version = triton.__version__
+        print(f"  ::  Detected Triton version: {version}")
+    except AttributeError:
+        print("  ::  Detected Triton version: no info")
+
     # This needs to be up here, so it can disable cudnn before anything can even think about using it
     torch.backends.cudnn.enabled = os.environ.get("TORCH_BACKENDS_CUDNN_ENABLED", "1").strip().lower() not in {"0", "off", "false", "disable", "disabled", "no"}
     if torch.backends.cudnn.enabled:
