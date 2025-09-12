df57ff67d9f53a7987c9cdc9b613dca3ca36815b
Rando717 <respect_killer@outlook.com>
2025-09-12 17:13:11 +0200

    Update comfyui-n.bat
    -same commits as with comfyui.bat (cleanup+more informative zluda check)

--- comfyui-n.bat
+++ comfyui-n.bat
@@ -1,44 +1,46 @@
 @echo off
 
-set FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE
-set FLASH_ATTENTION_TRITON_AMD_AUTOTUNE=TRUE
+set "FLASH_ATTENTION_TRITON_AMD_ENABLE=TRUE"
+set "FLASH_ATTENTION_TRITON_AMD_AUTOTUNE=TRUE"
 
-set MIOPEN_FIND_MODE=2
-set MIOPEN_LOG_LEVEL=3
+set "MIOPEN_FIND_MODE=2"
+set "MIOPEN_LOG_LEVEL=3"
 
-set PYTHON="%~dp0/venv/Scripts/python.exe"
-set GIT=
-set VENV_DIR=./venv
+set "PYTHON=%~dp0venv\Scripts\python.exe"
+set "GIT="
+set "VENV_DIR=.\venv"
 
-set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
+set "COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9"
 
 :: in the comfyui-user.bat remove the dots on the line below and change the gfx1030 to your gpu's specific code. 
 :: you can find out about yours here, https://llvm.org/docs/AMDGPUUsage.html#processors
 
-:: Set TRITON_OVERRIDE_ARCH=gfx1030
+:: set "TRITON_OVERRIDE_ARCH=gfx1030"
 
-set ZLUDA_COMGR_LOG_LEVEL=1
+set "ZLUDA_COMGR_LOG_LEVEL=1"
 
-echo *** Checking and updating to new version if possible 
+echo ** Checking and updating to new version if possible
 
 copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
 git pull
 copy comfy\customzluda\zluda.py comfy\zluda.py /y >NUL
 
-:: Check for zluda.exe and nccl.dll
+rem Check for zluda.exe and nccl.dll inside the zluda folder
 echo.
-echo ** Checking ZLUDA version
-pushd .
-cd ./zluda
+echo ** Checking ZLUDA installation...
+pushd .\zluda
 if exist zluda.exe (
-	if not exist nccl.dll (
-	echo nccl.dll is missing, likely blocked by AV as false positive.
+    if not exist nccl.dll (
+        for /f "tokens=2 delims= " %%v in ('zluda.exe --version 2^>^&1') do (
+            echo ** [FAIL] Detected version [%%v], but nccl.dll is missing. Likely blocked by AV as false positive.
+        )
+    ) else (
+        for /f "tokens=2 delims= " %%v in ('zluda.exe --version 2^>^&1') do (
+            echo ** [PASS] Detected version: %%v
+        )
+    )
 ) else (
-zluda.exe --version
-echo Check passed.
-)
-) else (
-	echo Check failed. Can't detect ZLUDA.
+    echo ** [FAIL] Can't detect zluda.exe inside .\zluda directory.
 )
 popd
 
