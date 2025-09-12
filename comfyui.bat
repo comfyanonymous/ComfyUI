f618968cbf3e968f7a2cd7749a8872262be6a5f1
Rando717 <respect_killer@outlook.com>
2025-09-12 17:00:00 +0200

    Update comfyui.bat
    -minor batch cleanup
    
    -slightly more informative zluda check

--- comfyui.bat
+++ comfyui.bat
@@ -1,37 +1,38 @@
 @echo off
 
-set PYTHON="%~dp0/venv/Scripts/python.exe"
-set GIT=
-set VENV_DIR=./venv
+set "PYTHON=%~dp0venv\Scripts\python.exe"
+set "GIT="
+set "VENV_DIR=.\venv"
 
-set COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9
+set "COMMANDLINE_ARGS=--auto-launch --use-quad-cross-attention --reserve-vram 0.9"
 
-set ZLUDA_COMGR_LOG_LEVEL=1
+set "ZLUDA_COMGR_LOG_LEVEL=1"
 
-echo *** Checking and updating to new version if possible 
+echo ** Checking and updating to new version if possible
 
 copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
 git pull
 copy comfy\customzluda\zluda-default.py comfy\zluda.py /y >NUL
 
-:: Check for zluda.exe and nccl.dll
+rem Check for zluda.exe and nccl.dll inside the zluda folder
 echo.
-echo ** Checking ZLUDA version
-pushd .
-cd ./zluda
+echo ** Checking ZLUDA installation...
+pushd .\zluda
 if exist zluda.exe (
-	if not exist nccl.dll (
-	echo nccl.dll is missing, likely blocked by AV as false positive.
+    if not exist nccl.dll (
+        for /f "tokens=2 delims= " %%v in ('zluda.exe --version 2^>^&1') do (
+            echo ** [FAIL] Detected version [%%v], but nccl.dll is missing. Likely blocked by AV as false positive.
+        )
+    ) else (
+        for /f "tokens=2 delims= " %%v in ('zluda.exe --version 2^>^&1') do (
+            echo ** [PASS] Detected version: %%v
+        )
+    )
 ) else (
-zluda.exe --version
-echo Check passed.
-)
-) else (
-	echo Check failed. Can't detect ZLUDA.
+    echo ** [FAIL] Can't detect zluda.exe inside .\zluda directory.
 )
 popd
 
 echo.
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
 pause
-
