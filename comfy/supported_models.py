633ca763a24d2fd90fc7960a6a049aed970817ed
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2025-09-20 19:09:35 -0700

    Lower wan memory estimation value a bit. (#9964)
    Previous pr reduced the peak memory requirement.

--- comfy/supported_models.py
+++ comfy/supported_models.py
@@ -995,7 +995,7 @@ class WAN21_T2V(supported_models_base.BASE):
     unet_extra_config = {}
     latent_format = latent_formats.Wan21
 
-    memory_usage_factor = 1.0
+    memory_usage_factor = 0.9
 
     supported_inference_dtypes = [torch.float16, torch.bfloat16, torch.float32]
 
@@ -1004,7 +1004,7 @@ class WAN21_T2V(supported_models_base.BASE):
 
     def __init__(self, unet_config):
         super().__init__(unet_config)
-        self.memory_usage_factor = self.unet_config.get("dim", 2000) / 2000
+        self.memory_usage_factor = self.unet_config.get("dim", 2000) / 2222
 
     def get_model(self, state_dict, prefix="", device=None):
         out = model_base.WAN21(self, device=device)
