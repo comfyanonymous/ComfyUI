4eb2367a703cf815b8355cb1b01f0cba277bf745
rattus128 <46076784+rattus128@users.noreply.github.com>
2025-09-28 08:14:16 +1000

    Reduce Peak WAN inference VRAM usage - part II (#10062)
    * flux: math: Use _addcmul to avoid expensive VRAM intermediate
    
    The rope process can be the VRAM peak and this intermediate
    for the addition result before releasing the original can OOM.
    addcmul_ it.
    
    * wan: Delete the self attention before cross attention
    
    This saves VRAM when the cross attention and FFN are in play as the
    VRAM peak.

--- comfy/ldm/flux/math.py
+++ comfy/ldm/flux/math.py
@@ -37,7 +37,10 @@ def rope(pos: Tensor, dim: int, theta: int) -> Tensor:
 
 def apply_rope1(x: Tensor, freqs_cis: Tensor):
     x_ = x.to(dtype=freqs_cis.dtype).reshape(*x.shape[:-1], -1, 1, 2)
-    x_out = freqs_cis[..., 0] * x_[..., 0] + freqs_cis[..., 1] * x_[..., 1]
+
+    x_out = freqs_cis[..., 0] * x_[..., 0]
+    x_out.addcmul_(freqs_cis[..., 1], x_[..., 1])
+
     return x_out.reshape(*x.shape).type_as(x)
 
 def apply_rope(xq: Tensor, xk: Tensor, freqs_cis: Tensor):
