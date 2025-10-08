862e7784f4700a6aea0da9c875af24990833adc6
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-10-08 14:40:41 +0600

    feat(gds): add nodes_gds.py to built-in extra nodes initialization

--- nodes.py
+++ nodes.py
@@ -2331,6 +2331,7 @@ async def init_builtin_extra_nodes():
         "nodes_model_patch.py",
         "nodes_easycache.py",
         "nodes_audio_encoder.py",
+        "nodes_gds.py",
     ]
 
     import_failed = []
