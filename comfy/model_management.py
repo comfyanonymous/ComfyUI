fa19dd46200e5708f0e17e24622939257bfcffca
strint <xiaoyulink@gmail.com>
2025-10-16 17:00:47 +0800

    debug offload

--- comfy/model_management.py
+++ comfy/model_management.py
@@ -516,6 +516,9 @@ class LoadedModel:
         logging.info(f"offload_device: {self.model.offload_device}")
         available_memory = get_free_memory(self.model.offload_device)
         logging.info(f"before unload, available_memory of offload device {self.model.offload_device}: {available_memory/(1024*1024*1024)} GB")
+        if available_memory < memory_to_free:
+            logging.error(f"Not enough cpu memory to unload. Available: {available_memory/(1024*1024*1024)} GB, Required: {memory_to_free/(1024*1024*1024)} GB")
+            return False
         try:
             if memory_to_free is not None:
                 if memory_to_free < self.model.loaded_size():
