b349dcb8e1cd68ea83b102514090d1f6b1381bca
rattus <46076784+rattus128@users.noreply.github.com>
2025-10-30 07:17:46 +1000

    Fix Race condition in --async-offload that can cause corruption (#10501)
    * mm: factor out the current stream getter
    
    Make this a reusable function.
    
    * ops: sync the offload stream with the consumption of w&b
    
    This sync is nessacary as pytorch will queue cuda async frees on the
    same stream as created to tensor. In the case of async offload, this
    will be on the offload stream.
    
    Weights and biases can go out of scope in python which then
    triggers the pytorch garbage collector to queue the free operation on
    the offload stream possible before the compute stream has used the
    weight. This causes a use after free on weight data leading to total
    corruption of some workflows.
    
    So sync the offload stream with the compute stream after the weight
    has been used so the free has to wait for the weight to be used.
    
    The cast_bias_weight is extended in a backwards compatible way with
    the new behaviour opt-in on a defaulted parameter. This handles
    custom node packs calling cast_bias_weight and defeatures
    async-offload for them (as they do not handle the race).
    
    The pattern is now:
    
    cast_bias_weight(... , offloadable=True) #This might be offloaded
    thing(weight, bias, ...)
    uncast_bias_weight(...)
    
    * controlnet: adopt new cast_bias_weight synchronization scheme
    
    This is nessacary for safe async weight offloading.
    
    * mm: sync the last stream in the queue, not the next
    
    Currently this peeks ahead to sync the next stream in the queue of
    streams with the compute stream. This doesnt allow a lot of
    parallelization, as then end result is you can only get one weight load
    ahead regardless of how many streams you have.
    
    Rotate the loop logic here to synchronize the end of the queue before
    returning the next stream. This allows weights to be loaded ahead of the
    compute streams position.

--- comfy/controlnet.py
+++ comfy/controlnet.py
@@ -310,11 +310,13 @@ class ControlLoraOps:
             self.bias = None
 
         def forward(self, input):
-            weight, bias = comfy.ops.cast_bias_weight(self, input)
+            weight, bias, offload_stream = comfy.ops.cast_bias_weight(self, input, offloadable=True)
             if self.up is not None:
-                return torch.nn.functional.linear(input, weight + (torch.mm(self.up.flatten(start_dim=1), self.down.flatten(start_dim=1))).reshape(self.weight.shape).type(input.dtype), bias)
+                x = torch.nn.functional.linear(input, weight + (torch.mm(self.up.flatten(start_dim=1), self.down.flatten(start_dim=1))).reshape(self.weight.shape).type(input.dtype), bias)
             else:
-                return torch.nn.functional.linear(input, weight, bias)
+                x = torch.nn.functional.linear(input, weight, bias)
+            comfy.ops.uncast_bias_weight(self, weight, bias, offload_stream)
+            return x
 
     class Conv2d(torch.nn.Module, comfy.ops.CastWeightBiasOp):
         def __init__(
@@ -350,12 +352,13 @@ class ControlLoraOps:
 
 
         def forward(self, input):
-            weight, bias = comfy.ops.cast_bias_weight(self, input)
+            weight, bias, offload_stream = comfy.ops.cast_bias_weight(self, input, offloadable=True)
             if self.up is not None:
-                return torch.nn.functional.conv2d(input, weight + (torch.mm(self.up.flatten(start_dim=1), self.down.flatten(start_dim=1))).reshape(self.weight.shape).type(input.dtype), bias, self.stride, self.padding, self.dilation, self.groups)
+                x = torch.nn.functional.conv2d(input, weight + (torch.mm(self.up.flatten(start_dim=1), self.down.flatten(start_dim=1))).reshape(self.weight.shape).type(input.dtype), bias, self.stride, self.padding, self.dilation, self.groups)
             else:
-                return torch.nn.functional.conv2d(input, weight, bias, self.stride, self.padding, self.dilation, self.groups)
-
+                x = torch.nn.functional.conv2d(input, weight, bias, self.stride, self.padding, self.dilation, self.groups)
+            comfy.ops.uncast_bias_weight(self, weight, bias, offload_stream)
+            return x
 
 class ControlLora(ControlNet):
     def __init__(self, control_weights, global_average_pooling=False, model_options={}): #TODO? model_options
