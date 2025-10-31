2a8610227dbcb937cd6018be98310b5343a46e4e
comfyanonymous <comfyanonymous@protonmail.com>
2025-10-30 22:07:27 -0400

    Add a ScaleROPE node. Currently only works on WAN models.

--- nodes.py
+++ nodes.py
@@ -2329,6 +2329,7 @@ async def init_builtin_extra_nodes():
         "nodes_model_patch.py",
         "nodes_easycache.py",
         "nodes_audio_encoder.py",
+        "nodes_rope.py",
     ]
 
     import_failed = []
