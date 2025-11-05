cb97b94ad9b1079b077121c298334afa42d9d23f
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2025-11-04 18:15:54 -0800

    Unclear why this is throwing linting errors

--- comfy/distributed/executors.py
+++ comfy/distributed/executors.py
@@ -13,7 +13,7 @@ class ContextVarExecutor(ThreadPoolExecutor):
     def submit(self, fn: typing.Callable, *args, **kwargs) -> Future:
         ctx = contextvars.copy_context()  # type: contextvars.Context
 
-        return super().submit(partial(ctx.run, partial(fn, *args, **kwargs)))
+        return super().submit(partial(ctx.run, partial(fn, *args, **kwargs)))  # pylint: disable=no-member
 
 
 class ContextVarProcessPoolExecutor(ProcessPoolExecutor):
