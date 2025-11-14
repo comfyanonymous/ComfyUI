c6414ed18265f944a1e506e2d77a9206fff21cdd
comfyanonymous <comfyanonymous@protonmail.com>
2025-11-14 01:20:34 -0500

    ruff.

--- comfy/ldm/chroma/layers.py
+++ comfy/ldm/chroma/layers.py
@@ -1,12 +1,9 @@
 import torch
 from torch import Tensor, nn
 
-from comfy.ldm.flux.math import attention
 from comfy.ldm.flux.layers import (
     MLPEmbedder,
     RMSNorm,
-    QKNorm,
-    SelfAttention,
     ModulationOut,
 )
 
