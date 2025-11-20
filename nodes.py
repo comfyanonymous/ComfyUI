06f7943d8c03d2557d7727191d94ba609539c9ee
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2025-11-20 12:08:03 -0800

    Fix ImageBatch with different channel count. (#10815)

--- nodes.py
+++ nodes.py
@@ -1852,6 +1852,10 @@ class ImageBatch:
     CATEGORY = "image"
 
     def batch(self, image1, image2):
+        if image1.shape[-1] != image2.shape[-1]:
+            channels = min(image1.shape[-1], image2.shape[-1])
+            image1 = image1[..., :channels]
+            image2 = image2[..., :channels]
         if image1.shape[1:] != image2.shape[1:]:
             image2 = comfy.utils.common_upscale(image2.movedim(-1,1), image1.shape[2], image1.shape[1], "bilinear", "center").movedim(1,-1)
         s = torch.cat((image1, image2), dim=0)
