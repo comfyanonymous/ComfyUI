546bea50f0d9a8b81ee71ea673ec288115e2853e
Jim Heising <jheising@gmail.com>
2025-12-02 19:29:27 -0800

    Added PATCH method to CORS headers (#11066)
    Added PATCH http method to access-control-allow-header-methods header because there are now PATCH endpoints exposed in the API.
    
    See https://github.com/comfyanonymous/ComfyUI/blob/1140c51a223c4718e7ca91dfa2b772ff1f4f48c3/api_server/routes/internal/internal_routes.py#L34 for an example of an API endpoint that uses the PATCH method.

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ def create_cors_middleware(allowed_origin: str):
             response = await handler(request)
 
         response.headers['Access-Control-Allow-Origin'] = allowed_origin
-        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, PUT, OPTIONS, PATCH'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response
