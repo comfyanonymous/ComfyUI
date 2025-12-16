f39c9caa554c37087992d2d64c888635ae5d1e67
Terry Jia <terryjia88@gmail.com>
2025-12-15 21:10:43 -0500

    add node - image compare

--- nodes.py
+++ nodes.py
@@ -2360,6 +2360,7 @@ async def init_builtin_extra_nodes():
         "nodes_nop.py",
         "nodes_kandinsky5.py",
         "nodes_wanmove.py",
+        "nodes_image_compare.py",
     ]
 
     import_failed = []
