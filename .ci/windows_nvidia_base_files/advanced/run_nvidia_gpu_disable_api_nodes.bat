5a4d012302847cfb0e8f4bb50dca5fc30e8ab585
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2025-12-26 15:21:18 -0800

    remove these ci files

--- .ci/windows_nvidia_base_files/advanced/run_nvidia_gpu_disable_api_nodes.bat
+++
@@ -1,3 +0,0 @@
-..\python_embeded\python.exe -s ..\ComfyUI\main.py --windows-standalone-build --disable-api-nodes
-echo If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest.
-pause
