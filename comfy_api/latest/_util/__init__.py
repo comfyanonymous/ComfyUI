eff4ea0b625e851d641b8f6532ff7afe2df16b9d
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2025-12-27 05:39:02 +0200

    [V3] converted nodes_images.py to V3 schema (#11206)
    * converted nodes_images.py to V3 schema
    
    * fix test

--- comfy_api/latest/_util/__init__.py
+++ comfy_api/latest/_util/__init__.py
@@ -1,5 +1,6 @@
 from .video_types import VideoContainer, VideoCodec, VideoComponents
 from .geometry_types import VOXEL, MESH
+from .image_types import SVG
 
 __all__ = [
     # Utility Types
@@ -8,4 +9,5 @@ __all__ = [
     "VideoComponents",
     "VOXEL",
     "MESH",
+    "SVG",
 ]
