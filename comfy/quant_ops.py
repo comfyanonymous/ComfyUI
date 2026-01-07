30088fa5a039ebef1874727aa47b782fdf815883
comfyanonymous <121283862+comfyanonymous@users.noreply.github.com>
2026-01-06 21:22:55 -0500

    Disable comfy kitchen cuda if pytorch cuda less than 13

--- comfy/quant_ops.py
+++ comfy/quant_ops.py
@@ -13,6 +13,13 @@ try:
         get_layout_class,
     )
     _CK_AVAILABLE = True
+    if torch.version.cuda is None:
+        ck.registry.disable("cuda")
+    else:
+        cuda_version = tuple(map(int, str(torch.version.cuda).split('.')))
+        if cuda_version < (13,):
+            ck.registry.disable("cuda")
+
     ck.registry.disable("triton")
     for k, v in ck.list_backends().items():
         logging.info(f"Found comfy_kitchen backend {k}: {v}")
