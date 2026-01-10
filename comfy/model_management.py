b0c14acf1e88ed3f1a36f5a0e58c6927f6eab0b0
DELUXA <djernovevo@gmail.com>
2026-01-10 23:51:05 +0200

    pythorch_attn_by_def_on_gfx1200 (#11793)

--- comfy/model_management.py
+++ comfy/model_management.py
@@ -368,7 +368,7 @@ try:
                     if any((a in arch) for a in ["gfx90a", "gfx942", "gfx1100", "gfx1101", "gfx1151"]):  # TODO: more arches, TODO: gfx950
                         ENABLE_PYTORCH_ATTENTION = True
                 if rocm_version >= (7, 0):
-                   if any((a in arch) for a in ["gfx1201"]):
+                   if any((a in arch) for a in ["gfx1200", "gfx1201"]):
                        ENABLE_PYTORCH_ATTENTION = True
         if torch_version_numeric >= (2, 7) and rocm_version >= (6, 4):
             if any((a in arch) for a in ["gfx1200", "gfx1201", "gfx950"]):  # TODO: more arches, "gfx942" gives error on pytorch nightly 2.10 1013 rocm7.0
