9f701f69dc8cc97d82b9b304e7d7ab79539f040d
Rattus <rattus128@gmail.com>
2026-01-13 21:13:35 +1000

    sd: empty cache on tiler fallback
    This is needed for aimdo where the cache cant self recover from
    fragmentation. It is however a good thing to do anyway after an OOM
    so make it unconditional.

--- comfy/sd.py
+++ comfy/sd.py
@@ -920,6 +920,7 @@ class VAE:
             do_tile = True
 
         if do_tile:
+            torch.cuda.empty_cache()
             dims = samples_in.ndim - 2
             if dims == 1 or self.extra_1d_channel is not None:
                 pixel_samples = self.decode_tiled_1d(samples_in)
@@ -995,6 +996,7 @@ class VAE:
             do_tile = True
 
         if do_tile:
+            torch.cuda.empty_cache()
             if self.latent_dim == 3:
                 tile = 256
                 overlap = tile // 4
