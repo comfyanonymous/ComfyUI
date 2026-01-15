b0580b83932360860c5f49522126889a80e93c70
Rattus <rattus128@gmail.com>
2026-01-16 01:34:12 +1000

    remove junk arg

--- cuda_malloc.py
+++ cuda_malloc.py
@@ -87,7 +87,7 @@ if not args.cuda_malloc:
     except:
         pass
 
-if enables_dynamic_vram() and comfy_aimdo.control.init(0):
+if enables_dynamic_vram() and comfy_aimdo.control.init():
     args.cuda_malloc = False
     os.environ['PYTORCH_CUDA_ALLOC_CONF'] = ""
 
