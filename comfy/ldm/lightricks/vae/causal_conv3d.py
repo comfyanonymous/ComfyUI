f3377052855db27dcf954b1f20902a4d6ae06542
rattus <46076784+rattus128@users.noreply.github.com>
2026-01-22 13:54:18 -0800

    Reduce LTX2 VAE VRAM consumption (#12028)
    * causal_video_ae: Remove attention ResNet
    
    This attention_head_dim argument does not exist on this constructor so
    this is dead code. Remove as generic attention mid VAE conflicts with
    temporal roll.
    
    * ltx-vae: consoldate causal/non-causal code paths
    
    * ltx-vae: add cache rolling adder
    
    * ltx-vae: use cached adder for resnet
    
    * ltx-vae: Implement rolling VAE
    
    Implement a temporal rolling VAE for the LTX2 VAE.
    
    Usually when doing temporal rolling VAEs you can just chunk on time relying
    on causality and cache behind you as you go. The LTX VAE is however
    non-causal.
    
    So go whole hog and implement per layer run ahead and backpressure between
    the decoder layers using recursive state beween the layers.
    
    Operations are ammended with temporal_cache_state{} which they can use to
    hold any state then need for partial execution. Convolutions cache their
    inputs behind the up to N-1 frames, and skip connections need to cache the
    mismatch between convolution input and output that happens due to missing
    future (non-causal) input.
    
    Each call to run_up() processes a layer accross a range on input that
    may or may not be complete. It goes depth first to process as much as
    possible to try and digest frames to the final output ASAP. If layers run
    out of input due to convolution losses, they simply return without action
    effectively applying back-pressure to the earlier layers. As the earlier
    layers do more work and caller deeper, the partial states are reconciled
    and output continues to digest depth first as much as possible.
    
    Chunking is done using a size quota rather than a fixed frame length and
    any layer can initiate chunking, and multiple layers can chunk at different
    granulatiries. This remove the old limitation of always having to process
    1 latent frame to entirety and having to hold 8 full decoded frames as
    the VRAM peak.

--- comfy/ldm/lightricks/vae/causal_conv3d.py
+++ comfy/ldm/lightricks/vae/causal_conv3d.py
@@ -1,10 +1,10 @@
 from typing import Tuple, Union
 
+import threading
 import torch
 import torch.nn as nn
 import comfy.ops
 ops = comfy.ops.disable_weight_init
-
 
 class CausalConv3d(nn.Module):
     def __init__(
@@ -42,23 +42,34 @@ class CausalConv3d(nn.Module):
             padding_mode=spatial_padding_mode,
             groups=groups,
         )
+        self.temporal_cache_state={}
 
     def forward(self, x, causal: bool = True):
-        if causal:
-            first_frame_pad = x[:, :, :1, :, :].repeat(
-                (1, 1, self.time_kernel_size - 1, 1, 1)
-            )
-            x = torch.concatenate((first_frame_pad, x), dim=2)
-        else:
-            first_frame_pad = x[:, :, :1, :, :].repeat(
-                (1, 1, (self.time_kernel_size - 1) // 2, 1, 1)
-            )
-            last_frame_pad = x[:, :, -1:, :, :].repeat(
-                (1, 1, (self.time_kernel_size - 1) // 2, 1, 1)
-            )
-            x = torch.concatenate((first_frame_pad, x, last_frame_pad), dim=2)
-        x = self.conv(x)
-        return x
+        tid = threading.get_ident()
+
+        cached, is_end = self.temporal_cache_state.get(tid, (None, False))
+        if cached is None:
+            padding_length = self.time_kernel_size - 1
+            if not causal:
+                padding_length = padding_length // 2
+            if x.shape[2] == 0:
+                return x
+            cached = x[:, :, :1, :, :].repeat((1, 1, padding_length, 1, 1))
+        pieces = [ cached, x ]
+        if is_end and not causal:
+            pieces.append(x[:, :, -1:, :, :].repeat((1, 1, (self.time_kernel_size - 1) // 2, 1, 1)))
+
+        needs_caching = not is_end
+        if needs_caching and x.shape[2] >= self.time_kernel_size - 1:
+            needs_caching = False
+            self.temporal_cache_state[tid] = (x[:, :, -(self.time_kernel_size - 1):, :, :], False)
+
+        x = torch.cat(pieces, dim=2)
+
+        if needs_caching:
+            self.temporal_cache_state[tid] = (x[:, :, -(self.time_kernel_size - 1):, :, :], False)
+
+        return self.conv(x) if x.shape[2] >= self.time_kernel_size else x[:, :, :0, :, :]
 
     @property
     def weight(self):
