1a0a869a16a94d1c100bca4ccffcb11b3151b525
Simon Risberg <simon@risberg.eu>
2026-01-30 17:29:17 +0100

    Remove workflows

---
+++ start.sh
@@ -0,0 +1,3 @@
+# pip install https://github.com/simonri/flash-attention-3-h100/releases/download/v1.0.0/flash_attn_3-3.0.0b1-cp39-abi3-linux_x86_64.whl
+
+python main.py --disable-auto-launch --disable-metadata --listen
