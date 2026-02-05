1f6b5670ff49acc9ec0ec4222bdbb6001ad8e825
AustinMroz <austin@comfy.org>
2026-02-04 22:18:21 -0800

    Add a Create List node (#12173)

--- nodes.py
+++ nodes.py
@@ -2433,7 +2433,8 @@ async def init_builtin_extra_nodes():
         "nodes_image_compare.py",
         "nodes_zimage.py",
         "nodes_lora_debug.py",
-        "nodes_color.py"
+        "nodes_color.py",
+        "nodes_toolkit.py",
     ]
 
     import_failed = []
