f4f7774aa800fe75fd78dc291e6e78d06bd5191c
tom-m <61305041+Tom-M-Git@users.noreply.github.com>
2026-05-18 03:14:44 +0900

    fix: prevent user-site package leakage

--- .ci/windows_nvidia_base_files/run_nvidia_gpu.bat
+++ .ci/windows_nvidia_base_files/run_nvidia_gpu.bat
@@ -1,3 +1,15 @@
+set "BASE=%~dp0"
+
+set PYTHONNOUSERSITE=1
+set PYTHONPATH=
+set PYTHONHOME=
+
+set "USERPROFILE=%BASE%user_profile_cache"
+set "HOME=%BASE%user_profile_cache"
+
+set "HF_HOME=%BASE%user_profile_cache\huggingface"
+set "TORCH_HOME=%BASE%user_profile_cache\torch"
+
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
 echo If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest. If you get a c10.dll error you need to install vc redist that you can find: https://aka.ms/vc14/vc_redist.x64.exe
 pause
