2a26fb51bc17c078e47f77239bda8452c3316948
tom-m <61305041+Tom-M-Git@users.noreply.github.com>
2026-05-18 12:28:05 +0900

    removed cache dir reset and make code more consistent

--- .ci/windows_amd_base_files/run_amd_gpu_enable_dynamic_vram.bat
+++ .ci/windows_amd_base_files/run_amd_gpu_enable_dynamic_vram.bat
@@ -1,14 +1,8 @@
 set "BASE=%~dp0"
 
-set PYTHONNOUSERSITE=1
-set PYTHONPATH=
-set PYTHONHOME=
-
-set "USERPROFILE=%BASE%user_profile_cache"
-set "HOME=%BASE%user_profile_cache"
-
-set "HF_HOME=%BASE%user_profile_cache\huggingface"
-set "TORCH_HOME=%BASE%user_profile_cache\torch"
+set "PYTHONNOUSERSITE=1"
+set "PYTHONPATH="
+set "PYTHONHOME="
 
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --enable-dynamic-vram
 pause
