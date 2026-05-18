21edf32dc67b3dbca86ddd4347f8d2fd63e99624
tom-m <61305041+Tom-M-Git@users.noreply.github.com>
2026-05-18 13:04:29 +0900

    Refactor batch script to use BASE variable

--- .ci/windows_nvidia_base_files/run_nvidia_gpu_fast_fp16_accumulation.bat
+++ .ci/windows_nvidia_base_files/run_nvidia_gpu_fast_fp16_accumulation.bat
@@ -4,6 +4,6 @@ set "PYTHONNOUSERSITE=1"
 set "PYTHONPATH="
 set "PYTHONHOME="
 
-.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --fast fp16_accumulation
+%BASE%python_embeded\python.exe -s %BASE%ComfyUI\main.py --windows-standalone-build --fast fp16_accumulation
 echo If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest. If you get a c10.dll error you need to install vc redist that you can find: https://aka.ms/vc14/vc_redist.x64.exe
 pause
