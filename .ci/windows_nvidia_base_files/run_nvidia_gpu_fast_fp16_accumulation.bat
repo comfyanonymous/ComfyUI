23efaf371ae461306a92d82904684ab3270717b8
tom-m <61305041+Tom-M-Git@users.noreply.github.com>
2026-05-18 12:25:28 +0900

    removed cache dir reset and make code more consistent

--- .ci/windows_nvidia_base_files/run_nvidia_gpu_fast_fp16_accumulation.bat
+++ .ci/windows_nvidia_base_files/run_nvidia_gpu_fast_fp16_accumulation.bat
@@ -1,14 +1,8 @@
 set "BASE=%~dp0"
 
-set PYTHONNOUSERSITE=1
-set PYTHONPATH=
-set PYTHONHOME=
-
-set "USERPROFILE=%BASE%user_profile_cache"
-set "HOME=%BASE%user_profile_cache"
-
-set "HF_HOME=%BASE%user_profile_cache\huggingface"
-set "TORCH_HOME=%BASE%user_profile_cache\torch"
+set "PYTHONNOUSERSITE=1"
+set "PYTHONPATH="
+set "PYTHONHOME="
 
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --fast fp16_accumulation
 echo If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest. If you get a c10.dll error you need to install vc redist that you can find: https://aka.ms/vc14/vc_redist.x64.exe
