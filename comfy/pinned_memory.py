4ee42d1568b922311bf73dc5c9e3e38eb93d632b
Rattus <rattus128@gmail.com>
2026-06-03 17:20:02 +1000

    pinned_memory: implement abs shortfall retry
    If pinned registration happens to fail despite the previous budget
    ensures, consider the allocation shortfall, ensure it again, and
    try again. This allows comfy pins to interoperate with other software
    that might be doing substantive pinning.

--- comfy/pinned_memory.py
+++ comfy/pinned_memory.py
@@ -96,6 +96,9 @@ def pin_memory(module, subset="weights", size=None):
         pin = comfy_aimdo.torch.hostbuf_to_tensor(hostbuf)[offset:offset + size]
         pin.untyped_storage()._comfy_hostbuf = hostbuf
         if torch.cuda.cudart().cudaHostRegister(pin.data_ptr(), size, 1) != 0:
+            comfy.model_management.discard_cuda_async_error()
+            comfy.model_management.free_registrations(size)
+            if torch.cuda.cudart().cudaHostRegister(pin.data_ptr(), size, 1) != 0:
                 comfy.model_management.discard_cuda_async_error()
                 del pin
                 hostbuf.truncate(offset, do_unregister=False)
