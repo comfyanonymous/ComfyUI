1091c47b3ae9ac423097649a5b7593d369d03bec
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2026-08-01 20:43:42 +0300

    [Partner Nodes] chore(Bria): increase price for video endpoints (#15186)
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- comfy_api_nodes/nodes_bria.py
+++ comfy_api_nodes/nodes_bria.py
@@ -289,7 +289,7 @@ class BriaRemoveVideoBackground(IO.ComfyNode):
             ],
             is_api_node=True,
             price_badge=IO.PriceBadge(
-                expr="""{"type":"usd","usd":0.005,"format":{"suffix":"/second"}}""",
+                expr="""{"type":"usd","usd":0.05,"format":{"suffix":"/second"}}""",
             ),
         )
 
@@ -357,7 +357,7 @@ class BriaVideoGreenScreen(IO.ComfyNode):
             ],
             is_api_node=True,
             price_badge=IO.PriceBadge(
-                expr="""{"type":"usd","usd":0.005,"format":{"suffix":"/second"}}""",
+                expr="""{"type":"usd","usd":0.05,"format":{"suffix":"/second"}}""",
             ),
         )
 
@@ -433,7 +433,7 @@ class BriaVideoReplaceBackground(IO.ComfyNode):
             ],
             is_api_node=True,
             price_badge=IO.PriceBadge(
-                expr="""{"type":"usd","usd":0.005,"format":{"suffix":"/second"}}""",
+                expr="""{"type":"usd","usd":0.05,"format":{"suffix":"/second"}}""",
             ),
         )
 
@@ -533,7 +533,7 @@ class BriaTransparentVideoBackground(IO.ComfyNode):
             ],
             is_api_node=True,
             price_badge=IO.PriceBadge(
-                expr="""{"type":"usd","usd":0.005,"format":{"suffix":"/second"}}""",
+                expr="""{"type":"usd","usd":0.05,"format":{"suffix":"/second"}}""",
             ),
         )
 
