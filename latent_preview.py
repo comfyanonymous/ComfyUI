890bb982321d1a3db8e16ed4a7f8b4c838f6bbcc
kijai <40791699+kijai@users.noreply.github.com>
2026-08-01 11:08:21 +0300

    Update latent_preview.py

--- latent_preview.py
+++ latent_preview.py
@@ -123,7 +123,7 @@ def prepare_callback(model, steps, x0_output_dict=None):
 
         preview_bytes = None
         if previewer:
-            if getattr(x0, "is_nested", False):
+            if x0.is_nested:
                 x0 = x0.tensors[0]
             preview_bytes = previewer.decode_latent_to_preview_image(preview_format, x0)
         pbar.update_absolute(step + 1, total_steps, preview_bytes)
