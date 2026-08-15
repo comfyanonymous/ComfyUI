3373199bf45a426e2cf9b62a238d6985173ced69
wunianze666-netizen <wunianze666@gmail.com>
2026-08-16 00:54:05 +0800

    docs: use documented API routes in script examples

--- script_examples/websockets_api_example_ws_images.py
+++ script_examples/websockets_api_example_ws_images.py
@@ -5,7 +5,6 @@ import websocket #NOTE: websocket-client (https://github.com/websocket-client/we
 import uuid
 import json
 import urllib.request
-import urllib.parse
 
 server_address = "127.0.0.1:8188"
 client_id = str(uuid.uuid4())
@@ -13,18 +12,8 @@ client_id = str(uuid.uuid4())
 def queue_prompt(prompt):
     p = {"prompt": prompt, "client_id": client_id}
     data = json.dumps(p).encode('utf-8')
-    req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
+    req =  urllib.request.Request("http://{}/api/prompt".format(server_address), data=data)
     return json.loads(urllib.request.urlopen(req).read())
-
-def get_image(filename, subfolder, folder_type):
-    data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
-    url_values = urllib.parse.urlencode(data)
-    with urllib.request.urlopen("http://{}/view?{}".format(server_address, url_values)) as response:
-        return response.read()
-
-def get_history(prompt_id):
-    with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
-        return json.loads(response.read())
 
 def get_images(ws, prompt):
     prompt_id = queue_prompt(prompt)['prompt_id']
