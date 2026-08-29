fb6f55bf0e868d15e705c4e7fab1b12f8d672cee
Pam <pamhome21@gmail.com>
2026-08-29 23:45:30 +0500

    Set disable_cfg1_optimization=True inside cfgpp_ud10_ab sampler

--- comfy/k_diffusion/sampling.py
+++ comfy/k_diffusion/sampling.py
@@ -434,7 +434,7 @@ def _sample_cfgpp_history(model, x, sigmas, extra_args=None, callback=None, disa
         return args["denoised"]
 
     model_options = extra_args.get("model_options", {}).copy()
-    extra_args["model_options"] = comfy.model_patcher.set_model_options_post_cfg_function(model_options, post_cfg_function)
+    extra_args["model_options"] = comfy.model_patcher.set_model_options_post_cfg_function(model_options, post_cfg_function, disable_cfg1_optimization=True)
 
     for i in trange(len(sigmas) - 1, disable=disable):
         denoised = model(x, sigmas[i] * s_in, **extra_args)
